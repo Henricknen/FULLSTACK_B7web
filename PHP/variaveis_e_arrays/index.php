ee67ac93324c22ed820dd586b27e7d829d394758
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-03 00:16:26 -0300

    fazendo_a_exibicao_de_uma…_frase_para_o_usuario_final_ultilizando_o_comando_'echo'

---
+++ PHP/variaveis_e_arrays/index.php
@@ -0,0 +1,3 @@
+<?php        // o que está dentro de '<?php' é codigo php
+    echo "ultilizando comando 'echo' que em PHP é usado para imprimir (ou exibir) uma ou mais expressões na saída do script";
+/* ?> */         // se o arquivo so conter 'php' o fechamento não é obrigado
