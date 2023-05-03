fbbbb50fef8f51ea5997064e656c23c0d77d1500
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-03 01:03:05 -0300

    criando_tipos_de_variaveis_int_float_string_boolean_null_'variavel'_e_um_valor_que_pode_variar

---
+++ PHP/variaveis_e_arrays/tipos_de_variaveis.php
@@ -0,0 +1,17 @@
+<?php
+$x = 10;        // criação da variável 'x', variável sempre será  precedida do símbolo'$' 
+echo  "A Soma de $x + 2 é: $x + 2 <br>";         // fazendo a soma da variável + 2
+
+$numero = 100;      // armazenamento de numero 'int'
+echo "O numero 'int' armazenado na variável numero foi: $numero <br>";
+
+$numeroQuebrado = 15.5;     // armazenando o número 'float'
+echo "O numero de 'ponto flutuante' armazendado em numeroQuebrado foi: $numeroQuebrado <br>";
+
+$nome = " Luis Henrique Siva Ferreira ";       // armazenamento de 'string' que é um texto entre aspas
+echo "O nome que está na variável é $nome e ela é do tipo 'string'";
+
+$estudadando = True;      // tipo 'booleano' True
+$dormindo = False;      // tipo 'booleano' False
+
+$vazio = null;      // 'null' significa que a variável existe mas não tem valor nenhum atribuído a ela e não precisa estar entre aspas ""
