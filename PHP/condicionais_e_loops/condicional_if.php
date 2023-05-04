d0b3a4397d728b5f1c3396920cc4209f09346777
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-04 08:36:10 -0300

    criando_uma_condicao_simples_ultilizando_'if'

---
+++ PHP/condicionais_e_loops/condicional_if.php
@@ -0,0 +1,8 @@
+<?php
+$idade = 30;
+
+if($idade < 18) {      // o que estiver entro das chaves '()' é a condição, que fará a execução de um dos códigos abaixo
+    echo 'Menor e idade';       // se condição for satisfeita
+} else {
+    echo 'Maior de idade';          // se 'if' condição não for satisfeita
+}
