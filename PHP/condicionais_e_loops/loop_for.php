c71829c3f9328b7949e5162f3ef0315c8edce1a0
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-06 01:39:25 -0300

    loop_'for'_e_mais_seguro_com_menos_chances_de_dar_poblemas_e_composto_de_uma_'inicializacao'_uma_'condicao'_e_um_'incremento'

---
+++ PHP/condicionais_e_loops/loop_for.php
@@ -0,0 +1,9 @@
+<?php
+for($numero = 0; $numero < 10; $numero += 1) {     // 1º etapa 'define'a variável $numero em 0, 2º etapa é uma 'condição' e 3º 'incrementa' o $numero
+    echo "for convencional: ". $numero. "<br>";
+}
+
+
+for($i = 0; $i < 10; $i++) {// simplificando, ultilizando variável auxiliar do 'for' com apenas uma letra
+    echo "for simplificado: ". $i. "<br>";
+}
