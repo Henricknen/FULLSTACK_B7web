44583c2cf17ae96199e706be22b473a316205314
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-06 00:41:25 -0300

    'loop_while'_e_uma_estrutura_de_repeticao_que_executa_um_bloco_de_codigo_enquanto_uma_condicao_especifica_for_verdadeira

---
+++ PHP/condicionais_e_loops/loop_while.php
@@ -0,0 +1,8 @@
+<?php
+$numero = 0;        // variável '$numero' iniçiada em  0
+
+while($numero < 10) {       // 'while 'enquanto a condição for sastisfeita
+    echo "N: ". $numero. "<br>";
+
+    $numero += 1;       // a cada execução do loop '$numero' será incrementado '+1' fazendo a variável '$numero' chegar a 10 que encerrará a repetição
+}
