dfb9f8f38d8aabaa954223e2fe86e7f4cc6fce26
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-07 00:40:02 -0300

    definindo_uma_'funcao'_que_e_um_bloco_de_codigo_nomeado_que_executa_uma_tarefa_especifica_podendo_ser_usada_varias_vezes

---
+++ PHP/funcoes_parametros_e_includes/funcao.php
@@ -0,0 +1,12 @@
+<?php
+function subsequente() {        // ultilizando 'fuction' para definir uma função chamada 'subsequente'
+    for($q = 0; $q < 10; $q++) {
+        echo $q. "<br>";
+    }
+
+    echo "<hr>";
+}
+
+subsequente();      // executando a função
+subsequente();          // executando a função novamente
+subsequente();
