8dcb61b99ccbcc28eff1152019653bbe7d8ad539
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-11 01:24:22 -0300

    criando_uma_funcao_recebe_uma_'string'_e_retorna_a_data_atual_e__o_dia_da_semana_em_portugues

---
+++ PHP/funcoes_parametros_e_includes/dia_da_semana.php
@@ -0,0 +1,14 @@
+<?php
+    $semana = array(
+        'Sun' => 'Domingo',
+        'Mon' => 'Segunda-Feira',
+        'Tue' => 'Terca-Feira',
+        'Wed' => 'Quarta-Feira',
+        'Thu' => 'Quinta-Feira',
+        'Fri' => 'Sexta-Feira',
+        'Sat' => 'Sábado'
+    );
+    $data = date('d/m/Y');
+    $data .= " - ";
+    $data .= $semana[date('D')];
+    echo $data;
