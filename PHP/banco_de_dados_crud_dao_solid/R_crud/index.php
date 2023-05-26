65b68129d2ac887093c67e9de7c200de23a0c466
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-26 01:58:55 -0300

    criando_o_'read'_fazendo_a_leitura_do_banco_de_dados_e_exibindo_todos_os_usuarios_cadastrados

---
+++ PHP/banco_de_dados_crud_dao_solid/R_crud/index.php
@@ -0,0 +1,31 @@
+<?php
+require 'config.php';
+
+$lista = [];        // array 'lista'
+$sql = $pdo-> query("SELECT * FROM usuarios");     // puxando os dados da tabela 'usuarios', para exibir todos os usúarios cadastrados
+if($sql-> rowCount() > 0) {     // verificando se há algum registro
+    $lista = $sql-> fetchAll(PDO:: FETCH_ASSOC);       // 'fetchAll' criará um array de registros que será adiçionado no array 'lista', 'PDO:: FETCH_ASSOC' faz as assoçiações de campos
+}
+?>
+
+<a href = "adicionar.php">ADIÇIONAR</a>        <!-- botão envia dados preenchidos pra o arquivos especificados em 'href' -->
+
+<table border = "1" width = "100%">
+    <tr>
+        <th> ID </th>
+        <th> NOME </th>
+        <th> E-MAIL </th>
+        <th> AÇÕES </th>
+    </tr>
+    <?php foreach($lista as $usuario): ?>
+        <tr>
+            <td><?php echo $usuario['id']; ?></td>
+            <td><?php echo $usuario['nome']; ?></td>
+            <td><?php echo $usuario['email']; ?></td>
+            <td>
+                <a href="editar.php? id = <?php echo $usuario['id']; ?>">[ Editar ]</a>     <!-- botão de ação 'editar' -->
+                <a href="excluir.php? id = <?php echo $usuario['id']; ?>">[ Excluir ]</a>       <!-- botão de ação 'excluir' -->
+            </td>
+        </tr>
+    <?php endforeach ?>
+</table>
