0d8da5a9780d10f6221a1f24e7721d7eda8736d1
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-05-26 00:55:12 -0300

    criando_verificacao_de_email_para_impedir_cadastramento_de_emails_iguais

--- PHP/banco_de_dados_crud_dao_solid/C_crud/adicionar_action.php
+++ PHP/banco_de_dados_crud_dao_solid/C_crud/adicionar_action.php
@@ -8,14 +8,22 @@ if($name && $email) {       // verificação se '$name' e '$email' eestão corre
 
     // $sql = $pdo-> query("INSERT INTO  usuarios (nome, email) VALUES ('$nome', '$email')");      // inserindo no banco de dados ultilizando método 'query'
 
-    $sql = $pdo-> prepare("INSERT INTO  usuarios (nome, email) VALUES (:name, :email)");        // inseção no banco ultilizzando método 'prepare'
-    $sql-> bindValue(':name', $name);     // assoçiações de cada item ultilizando método 'bindValue'
-    $sql-> bindValue(':email', $email);
-    $sql-> execute();       // fazendo a execução
+    $sql = $pdo-> prepare("SELECT * FROM usuarios WHERE email = :email");        // verificando se 'email' está cadastrado
+    $sql-> bindValue('email', $email);
+    $sql-> execute();
 
-    header("Location: index.php");      // 'depois de adiçionado é retornado para o 'index.php'
-    exit;
+    if($sql-> rowCount() === 0) {      // 'rowCount' mostra quantos regitros vieram da consulta
+        $sql = $pdo-> prepare("INSERT INTO  usuarios (nome, email) VALUES (:name, :email)");        // inseção no banco ultilizzando método 'prepare'
+        $sql-> bindValue(':name', $name);     // assoçiações de cada item ultilizando método 'bindValue'
+        $sql-> bindValue(':email', $email);
+        $sql-> execute();       // fazendo a execução
 
+        header("Location: index.php");      // 'depois de adiçionado é retornado para o 'index.php'
+        exit;
+    } else {
+        header("Location: adicionar.php");
+        exit;
+    }
 } else {        // se '$name' e '$email' não estiverem corretos
     header("Location: adicionar.php");      // 'header' faz voltar para a página espeçificada em 'Location'
     exit;
