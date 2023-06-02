393dc1c739781d138249582f5decc7e310a8725f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-06-02 01:32:14 -0300

    criando_um_'view'_dinamico

--- PHP/padrao_mvc/mvc/src/views/pages/home.php
+++ PHP/padrao_mvc/mvc/src/views/pages/home.php
@@ -1,3 +1,11 @@
 <?php $render('header'); ?>      <!-- '$render' puxa um arquivo parçial -->
 
-Algum conteudo
+Me chamo : <?php echo $nome; ?><br>
+tenho : <?php echo $idade; ?> anos
+
+<hr>
+
+<?php foreach($posts as $post); ?>
+<h3><?php echo $post['titulo']; ?></h3>
+<p><?php echo $post['corpo']; ?></p>
+<?php endforeach; ?>
