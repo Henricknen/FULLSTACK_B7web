ae0b7a6e5f4c059e2318204cdf2602805b52d4ac
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-06-07 13:21:14 -0300

    verificando_se_usuario_tem_algum_login_e_se_este_login_esta_valido
    verificando_se_usuario_tem_algum_login_e_se_este_login_esta_valido

---
+++ devsbook/src/controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+
+class LoginController extends Controller {
+
+    public function signin() {      // action de 'login'
+		echo 'login';
+	}
+
+    public function signup() {      // action de 'cadastro'
+		echo 'cadastro';
+	}
+
+}
