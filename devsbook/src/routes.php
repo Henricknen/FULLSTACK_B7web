f10d26fa8ba75a727792639e734fd504d5b2a68b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-06-09 12:15:10 -0300

    ..

--- devsbook/src/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-use core\Router;
-
-$router = new Router();
-
-$router->get('/', 'HomeController@index');      // rota padrão leva para 'HomeController' index
-$router->get('/login', 'LoginController@signin');       // se usuário não estiver logado irá para 'signin'
-$router->get('/cadastro', 'LoginController@signup');
