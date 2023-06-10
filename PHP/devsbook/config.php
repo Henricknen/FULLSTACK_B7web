617d5e0fac32f1fe693218e0e4c19257dc86f39f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-06-10 00:32:50 -0300

    criando_projeto_'devsbook'_iniciando_com_uma_'base'_criando_pastas_e_arquivo_'config.php'_de_conexao_com_banco_de_dados

---
+++ PHP/devsbook/config.php
@@ -0,0 +1,9 @@
+<?php
+$base = 'http://localhost/FULLSTACK_B7web/PHP/devsbook';     // base de 'url' do projeto
+
+$db_name = 'devsbook';      // dados do banco de dados
+$db_host = 'localhost';
+$db__user ='root';
+$db_pass = '';
+
+$pdo = new PDO("mysql:dbname=". $db_name. ";host=". $db_host, $db__user, $db_pass);       // instançia do 'PDO'
