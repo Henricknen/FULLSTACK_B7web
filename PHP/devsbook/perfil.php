69780904b43f7b1d63008fdf2329ee5db93a267d
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-06-21 00:31:34 -0300

    criando_tela_de_'fotos'

--- PHP/devsbook/perfil.php
+++ PHP/devsbook/perfil.php
@@ -146,13 +146,13 @@ require 'partials/menu.php';
                 <div class="box-body row m-20">
 
                     <?php if (count($user->photos) > 0) : ?>
-                        <?php foreach ($user->photos as $item) : ?>
+                        <?php foreach ($user->photos as $key=> $item) : ?>
                             <div class="user-photo-item">
-                                <a href="#modal-1" rel="modal:open">
+                                <a href="#modal-<?=$key;?>" rel="modal:open">
                                     <img src="<?= $base; ?>/media/uploads/<?= $item->body; ?>" />
                                 </a>
-                                <div id="modal" style="display:none">
-                                <img src="<?=$base?>/media/uploads/<?= $item->body; ?>">
+                                <div id="modal-<?=$key;?>" style="display:none">
+                                <img src="<?=$base;?>/media/uploads/<?= $item->body; ?>">
                                 </div>
                             </div>
                         <?php endforeach; ?>
