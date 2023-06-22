4698624661a65f239de584e7f0b3044491cd129a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-06-22 12:42:46 -0300

    preenchendo_as_informacoes_do_'form'_da_tela_de_configuracoes_com_as_informacoes_do_usuario

--- PHP/devsbook/configuracoes.php
+++ PHP/devsbook/configuracoes.php
@@ -19,38 +19,42 @@ require 'partials/menu.php';
     <form method = "POST" class = "config-form" enctype = "multipart/form-data" action = "configuracoes_action.php">
         <label>
             Novo Avatar: <br/>
-            <input type = "file" name = "avatar"/>      <!-- campo de trocar as fotos -->
+            <input type = "file" name = "avatar"/><br/>      <!-- campo de trocar as fotos -->
+
+            <img class="mini" src = "<?= $base; ?>/media/avatars/<?= $userInfo-> avatar; ?>"/>
         </label>
 
         <label>
             Nova Capa: <br/>
-            <input type = "file" name = "cover"/>      <!-- campo de trocar as fotos -->
+            <input type = "file" name = "cover"/><br/>      <!-- campo de trocar as fotos -->
+            
+            <img class="mini" src = "<?= $base; ?>/media/covers/<?= $userInfo-> cover; ?>"/>
         </label>
 
         <hr><!-- informações padrão do usuário -->
         <label>
             Nome completo: <br/>
-            <input type=  "text" name = "name"/>
+            <input type=  "text" name = "name" value = "<?= $userInfo-> name; ?>"/>
         </label>
         
         <label>
             E-mail: <br/>
-            <input type=  "email" name = "email"/>
+            <input type=  "email" name = "email" value = " <?= $userInfo-> email; ?>"/>
         </label>
 
         <label>
             Data de nascimento: <br/>
-            <input type=  "text" name = "birthdate"/>
+            <input type=  "text" id = "birthdate" name = "birthdate" value = "<?= date('d/m/Y', strtotime($userInfo-> birthdate)); ?>"/>       <!-- 'date('d/m/Y', strtotime())' é uma mascará que permite a formatação da data no padrão brasileiro -->
         </label>
 
         <label>
             Cidade: <br/>
-            <input type=  "text" name = "city"/>
+            <input type=  "text" name = "city" value = "<?= $userInfo-> city; ?>"/>
         </label>
         
         <label>
             Trabalho: <br/>
-            <input type=  "text" name = "work"/>
+            <input type=  "text" name = "work" value = "<?= $userInfo-> work; ?>"/>
         </label>
 
         <hr>
@@ -69,6 +73,13 @@ require 'partials/menu.php';
     </form>
 
 </section>
+<script src = "https://unpkg.com/imask"></script>       <!-- plugin javascript 'imask'  é usado para criar e aplicar máscaras em campos de entrada -->
+<script>
+    IMask(      // função 'IMask' ultiliza dois parâmentros
+        document.getElementById("birthdate"),       // 1º campo a qual será colcado o efeito
+        {mask: '00/00/0000'}        // 2º máscara de uma data
+    );
+</script>
 <?php
 require 'partials/footer.php';
 ?>
