5b7838e81847a8d3834617561b8cccdf76517fd4
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-02 16:46:20 -0300

    criando_'controller'

--- LARAVEL/projeto_laravel/routes/web.php
+++ LARAVEL/projeto_laravel/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {       // rota prinçipal do site
-    return view('nova_view');
-});
+Route::get('/', [SiteController:: class, 'index']);       // passando o 'controller' e o 'método' como parâmetro
+
+// Route::get('/', function () {       // rota prinçipal do site
+//     return 'Router';
+//     // return view('nova_view');
+// });
