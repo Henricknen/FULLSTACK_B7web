1e9777cbe14c1989b5549b2f162dd2924ad03030
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-03 00:17:58 -0300

    criando_'rota'_e_'view'_sair.blade.php

--- LARAVEL/projeto_laravel/app/Http/Controllers/SiteController.php
+++ LARAVEL/projeto_laravel/app/Http/Controllers/SiteController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;     // 'namespace' é o endereço da classe
 
 use Illuminate\Http\Request;
 
-class SiteController extends Controller     // classe 'SiteController' extende a classe 'Controller'
-{
-    //
+class SiteController extends Controller {     // classe 'SiteController' extende a classe 'Controller'
+    
     public function index() {      // método que vai reçeber esta requisição
-        $framework = "Laravel";
-        return 'Luis Henrique Silva Ferreira está codificando: '. $framework;
-        // return view('nova_view');       // retornando 'view'
-        // aqui seria possível criar uma lógica...
-        // verificar se um usuário existe
-        // buscar dados de um usuário, etc.
+    //     $framework = "Laravel";
+    //     return 'Luis Henrique Silva Ferreira está codificando: '. $framework;
+        return view('nova_view');       // retornando 'view'
+    }
+
+    public function exit() {
+        return view('sair');
     }
 }
--- LARAVEL/projeto_laravel/routes/web.php
+++ LARAVEL/projeto_laravel/routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController:: class, 'index']);       // passando o 'controller' e o 'método' como parâmetro
 
-// Route::get('/', function () {       // rota prinçipal do site
+Route::get('/sair', [SiteController:: class, 'exit']);     // criando rota '/sair' e apontando ela para o controller 'SiteController' e para o método'exit'
+
+// Route::get('/', function () {       // '/' rota prinçipal do site
 //     return 'Router';
 //     // return view('nova_view');
 // });
