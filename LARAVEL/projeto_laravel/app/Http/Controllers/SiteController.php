3ad9c8ebdb95a5316e48224103a91881fa9ddcfe
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-03 11:49:07 -0300

    passando_dados_do_'controller'_para_'view'

--- LARAVEL/projeto_laravel/app/Http/Controllers/SiteController.php
+++ LARAVEL/projeto_laravel/app/Http/Controllers/SiteController.php
@@ -7,9 +7,15 @@ use Illuminate\Http\Request;
 class SiteController extends Controller {     // classe 'SiteController' extende a classe 'Controller'
     
     public function index() {      // método que vai reçeber esta requisição
-    //     $framework = "Laravel";
-    //     return 'Luis Henrique Silva Ferreira está codificando: '. $framework;
-        return view('nova_view');       // retornando 'view'
+        $framework = "Laravel";
+        $ano = 2023;
+
+        $data = [       // array de dados com chaves e valores
+            'codificando_framework' => $framework,       // definindo o campo 'framework' com a variável '$framework'
+            'ano' => $ano
+        ];
+
+        return view('bem_vindo', $data);        // chamando a função 'view' e passando array '$data' para ser usado na visualização
     }
 
     public function exit() {
---
+++ LARAVEL/projeto_laravel/resources/views/bem_vindo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bem vindo</title>
+</head>
+<body>
+    <h1>Seja bem vindo ao meu Repositorio de {{$codificando_framework}}</h1>       <!-- para que o blade entenda que '$codificando_framework' é uma variável que vem do controller é necessario colocar entre chaves { duplas -->
+    <h2>Codificado no ano de {{$ano}}</h2>
+</body>
+</html>
