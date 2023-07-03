e4328072b7fcb35da5a32e1c56deff3ffa1cce1c
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-03 20:04:29 -0300

    passando_paramentros_para_as_'rotas'

--- LARAVEL/projeto_laravel/app/Http/Controllers/SiteController.php
+++ LARAVEL/projeto_laravel/app/Http/Controllers/SiteController.php
@@ -21,4 +21,12 @@ class SiteController extends Controller {     // classe 'SiteController' extende
     public function exit() {
         return view('sair');
     }
+
+    public function users(Request $r) {       // parametro '$r' do tipo 'request' permite que acesse os dados da requisicao 'http' incluindo os valores enviados pelo usuario
+
+        $data = [       // '$data' é um array associativo que armazena os dados a serao passados para a view
+            'quantidade' => $r -> qnt       //  armazena o valor da quantidade fornecida pelo usuario no array '$data'
+        ];
+        return view('usuarios', $data);
+    }
 }
--- LARAVEL/projeto_laravel/routes/web.php
+++ LARAVEL/projeto_laravel/routes/web.php
@@ -3,11 +3,9 @@
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [SiteController:: class, 'index']);       // passando o 'controller' e o 'método' como parâmetro
+Route::get('/', [SiteController:: class, 'index']);       // '/' define rota prinçipal do site e passando o 'controller' e o 'método' como parâmetro
 
-Route::get('/sair', [SiteController:: class, 'exit']);     // criando rota '/sair' e apontando ela para o controller 'SiteController' e para o método'exit'
+Route::get('/sair', [SiteController:: class, 'exit']);    // criando rota '/sair' e apontando ela para o controller 'SiteController' e para o método'exit'
 
-// Route::get('/', function () {       // '/' rota prinçipal do site
-//     return 'Router';
-//     // return view('nova_view');
-// });
+Route::get('/usuarios/{qnt}', [SiteController:: class, 'users']);     // endereço '/usuarios', '/{qnt}' indica que será passado algum valor pelo usuário e método 'users'
+
