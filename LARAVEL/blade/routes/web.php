24ff06cc6acee1afc7a7e65a5bbee164a39234ef
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-05 00:34:03 -0300

    removendo_diretorio

--- LARAVEL/blade/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\SiteController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [SiteController:: class, 'index']);       // '/' define rota prinçipal do site e passando o 'controller' e o 'método' como parâmetro
--- LARAVEL/condicionais_blade/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\CondicionalController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [CondicionalController:: class, 'index']);       // '/' define rota prinçipal do site e passando o 'controller' e o 'método' como parâmetro
