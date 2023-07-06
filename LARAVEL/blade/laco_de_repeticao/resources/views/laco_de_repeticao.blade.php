6ba8683f45b19ee11e56f32cb462dd9364b4aceb
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-06 00:38:24 -0300

    Utilizando_'@foreach'_para_o_laco_de_repeticao_'foreach'_e_'@for'_para_o_laco_de_repeticao_'for'_no_blade_o_mecanismo_de_template_do_'laravel'

---
+++ LARAVEL/blade/laco_de_repeticao/resources/views/laco_de_repeticao.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laço_de_repetição</title>
+</head>
+<body>
+    
+    @for($i = 0; $i <= 10 ;$i++)        {{-- laço de repetição 'for' no blade não se abre o bloco de chaves '{'  --}}
+        <p>Apresento número <b>de 1 a 10 </b></p>
+        <p>Número de valor: {{$i}}</p>
+    @endfor
+
+    <br><b>Ingredientes</b>
+    @foreach ($ingredientes as $ing)        {{-- percorrendo o array '$ingredientes' e chamando cada item que encontrar de '$ing' --}}
+        <p>{{$ing}}</p>
+    @endforeach
+
+</body>
+</html>
