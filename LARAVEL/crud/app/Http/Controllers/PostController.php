0197ba520106b85c842f6977fe4d69b1fbc3b9c8
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-11 00:15:53 -0300

    criando_controller_do_tipo_'resource'_que_contem_rotas_pre_implementadas_para_cada_uma_das_acoes_do_crud

--- LARAVEL/crud/app/Http/Controllers/PostController.php
+++ LARAVEL/crud/app/Http/Controllers/PostController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\models\Post;
+use App\Models\Post;
 
 class PostController extends Controller {
-    public function create(Request $request) {      // método 'create'
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() {
+        $posts = Post::all();       // buscando todos os 'posts'
+
+        return $posts;      // listando todos 'posts'
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() {
+        // no futuro irár apenas exibir o formulário
         $new_post = [       // 'post' será incluido no banco de dados
             'title' => 'Crud Laravel',
             'content' => 'Framework PHP',
             'author' => 'Luis Henrique Silva Ferreira'
         ];
-
-                // forma mais convençional de criar um registro no banco de dados
-        // $post = new Post($new_post);        // instançiando o model 'Post'
-        // $post-> save();     // 'save' salva os dados no banco de dados
 
         $post = new Post();
 
@@ -24,46 +33,50 @@ class PostController extends Controller {
         $post-> author = 'Luis Henrique Silva Ferreira';
         $post-> save();
 
-        dd($post);
+        return $post;
     }
 
-    public function read(Request $r) {     // fazendo a leitura do banco de dados
-        $post = new Post();     // instançiando 'model' post
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) {
+        // reçebera um post com um novo recurso
+    }
 
-        // $posts = $post-> all();     // método 'all' pega todos os posts cadastrados no banco de dados
-        // $posts = $post-> where('id', '=', 1);       // ultilizando condição 'where' para ler um post espeçifico
-        $post = $post-> find(1);       // ultilizando método 'find' para pegar a chave primaria que é definida na tabela posts que é o 'id'
+    /**
+     * Display the specified resource.
+     */
+    public function show($id) {
+        $post = new Post();
+
+        $post = $post-> find($id);
 
         return $post;
     }
 
-    public function all(Request $r) {       // método 'all'
-        $posts = Post::all();       // chamando diretamente todos 'posts'
-
-        return $posts;
-    }
-
-    public function update(Request $request) {
-        // $post = Post::find(2);      // atualiza 'post' de id 2
-        // $post-> title = 'Uma vez que tem este registro, ele pode ser modificado';        // 'title' será atualizada
-        // $post -> save();    # atualizando o registro no banco de dados
-
-        $posts = Post::where('id', '>', 0)-> update([          // atualizando mais de um 'post'
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id) {
+        $posts = Post::find($id)-> update([
             'author'=> 'Luis Henrique S F'
         ]);
         
         return $posts;
     }
 
-    public function delete(Request $request) {      // métod 'delete' apaga um registro do banco de dados
-        // $post= Post::find(1);       // seleçionando o registro que será apagado
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id) {
+        //
+    }
 
-        // if($post) {
-        //     return $post-> delete();       // deletando
-        // }
-
-        // return 'Não existe post com este id';
-
-        $post = Post::where('id', '>', 0)-> delete();     // faz a exclusão em massa deletando todos os registros do banco de dados
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id) {
+        $post = Post::find($id)-> delete();
+        return $post;
     }
 }
