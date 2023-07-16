43121509e5987788187c833f068cd55292bb38ae
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-16 00:14:35 -0300

    restringindo_'update'_e_'delete'_em_relacionamentos

--- LARAVEL/relations/database/migrations/2023_07_15_231017_foreignkey_users_address.php
+++ LARAVEL/relations/database/migrations/2023_07_15_231017_foreignkey_users_address.php
@@ -9,20 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::table('users', function (Blueprint $table) {
-            $table-> foreign('address_id')-> references('id')-> on('addresses');        // adiçionando uma 'foreign key'
+            $table-> foreign('address_id')        // adiçionando uma 'foreign key'
+            -> references('id')
+            -> on('addresses')
+            -> onDelete('SET NULL');        // quando for deletado o enderço que está referênçiado a um usuário, no mesmo será setado o valor de 'NULL'
+            // -> onDelete('CASCADE');      // se o endereço for deletado 'CASCADE' deleta tudo o que estiver relaçionado com ele
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table-> dropForeign('address_id');
         });
     }
 };
