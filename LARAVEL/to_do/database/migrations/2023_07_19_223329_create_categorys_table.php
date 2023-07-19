f11155a5443ea6b4f67f0fa61451824b08bc0591
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-19 20:09:16 -0300

    criacao_da_migration_'create_categorys_table'_para_fazer_relacionamentos

---
+++ LARAVEL/to_do/database/migrations/2023_07_19_223329_create_categorys_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('categorys', function (Blueprint $table) {       // criação da tabela 'categorys'
+            $table->id();
+            $table->string('color')->default('#FFFFFF');
+            $table->foreignIdFor(User::class)->references('id')->on('users');       // relaçionamento entr tabela "categorys" e a tabela "users"
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+
+        Schema::table('categorys', function(Blueprint $table) {     // deletando a ForeignIdFor chave 'estrangeira'
+            $table->dropForeignIdFor((User::class));
+        });
+
+        Schema::dropIfExists('categorys');
+    }
+};
