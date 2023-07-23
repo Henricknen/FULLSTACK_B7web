dc69b550ba9ed293e11b9c07e54d907fd8146677
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-23 16:48:25 -0300

    criando_'template'_do_projeto_e_fazendo_configuracoes_de_font_e_cores

---
+++ LARAVEL/to_do/resources/views/layouts/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoApp</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    a
+</body>
+</html>
