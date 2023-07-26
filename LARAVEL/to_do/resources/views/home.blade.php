0fa164b9696fd05899c8b979fbfe3dbffb2d7869
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-26 01:01:33 -0300

    componetizando_'layout.blade.php'_e_o_enserindo-o_na_pagina_principal

---
+++ LARAVEL/to_do/resources/views/home.blade.php
@@ -0,0 +1,54 @@
+<x-layout>      {{-- tudo o que for comum entre as páginas será renderizado pelo componente 'layout' --}}
+    <section class="graph">
+        <div class="graph_header">
+            <h2>Progresso do dia</h2>
+            <div class="graph_header-line"></div>
+            <div class="graph_header-date">
+                <img src="/assets/images/icon-prev.png">
+                    7/06
+                <img src="/assets/images/icon-next.png">
+            </div>
+        </div>
+
+        <div class="graph_header-subtitle">Tarefas: <b>3 / 6</b>
+        
+        </div>
+
+        <div class="graph-placeholder">
+
+        </div>
+
+        <div class="tasks_left_footer">
+            <img src="/assets/images/icon-info.png">
+            Resta apenas 3 tarefas para serem realizadas
+        </div>
+        
+    </section>
+
+    <section class="list">
+        <div class="list_header">
+            <select class="list_header-select">
+                <option value="1">Todas as tarefas</option>
+            </select>
+        </div>
+        <div class="task_list">
+            <div class="task">
+                <div class="title">
+                    <input type="checkbox"/>
+                    <div class="task_title">Titulo da Tarefa</div>
+                </div>
+                <div class="priority">
+                    <div class="sphere">Titulo da Tarefa</div>
+                </div>
+                <div class="actions">
+                    <a href="#">
+                        <img src="/assets/images/icon-edit.png">
+                    </a>
+                    <a href="#">
+                        <img src="/assets/images/icon-delete.png">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-layout>
