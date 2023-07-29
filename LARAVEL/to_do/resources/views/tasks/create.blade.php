31d0024cdc0654751a2ba8c23d99a9022faa72f6
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-29 00:51:52 -0300

    editando_pagina_de_criacao_de_nova_tarefa_criando_formulario_para_preencher_o_'nome'_'data'_'categoria'_e_'descricao'_da_nova_tarefa_que_sera_cadastrada

--- LARAVEL/to_do/resources/views/tasks/create.blade.php
+++ LARAVEL/to_do/resources/views/tasks/create.blade.php
@@ -12,14 +12,31 @@
                 <label for="title">
                     Título da task
                 </label>
-                <input name="title" placeholder="Digite o titulo da tarefa" required />
+                <input id="title" name="title" placeholder="Digite o titulo da tarefa" required />
+            </div>
+            
+            <div class="inputArea">
+                <label for="due_date">
+                    Data de realização
+                </label>
+                <input type="date" id="due_date" name="due_date" placeholder="Digite o titulo da tarefa" required />
+            </div>
+            
+            <div class="inputArea">
+                <label for="category">
+                    Categoria
+                </label>
+                <select id="category" name="category" required>       {{-- 'select' category é 'required' obrigatorio seu preenchimento --}}
+                    <option selected disabled value="">Seleçione a categoria</option>
+                    <option>Valor qualquer</option>
+                </select>
             </div>
             
             <div class="inputArea">
                 <label for="title">
-                    Título da task
+                    descrição da tarefa
                 </label>
-                <input name="title" placeholder="Digite o titulo da tarefa" required />
+                <textarea name="description" placeholder="Digite uma descrição para sua task"></textarea>       {{-- textarea não é obrigado ser preenchido --}}
             </div>
         </form>
     </section>
