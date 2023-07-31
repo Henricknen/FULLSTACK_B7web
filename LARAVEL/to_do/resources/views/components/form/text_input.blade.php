fd3640ee77cf053355614e0dfc2026133ee11bb7
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-07-31 19:28:09 -0300

    passando_dados_para_os_componentes_da_pagina_de_editar_tarefas

--- LARAVEL/to_do/resources/views/components/form/text_input.blade.php
+++ LARAVEL/to_do/resources/views/components/form/text_input.blade.php
@@ -6,5 +6,6 @@
     type="{{empty($type) ? 'text' : $type}}"        {{-- se 'type' não for definido será do tipo 'text' --}}
     id="{{$name}}" placeholder="{{$placeholder ?? ''}}"
     {{empty($required) ? '' : 'required'}}      {{-- verificando se 'required' está ou não está vazia --}}
+    value="{{$value ?? ''}}"        {{-- indicação ao componente que está sendo passada a variável '$value' ou vazio '' --}}
     />
 </div>
