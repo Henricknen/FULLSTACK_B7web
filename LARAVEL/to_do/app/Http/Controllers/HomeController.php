f6bf76fe36dc9e30bd3bd9550ee4abff70276929
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-04 20:58:45 -0300

    configurando_o_timezone_para_'America/Sao_Paulo'_e_filtrando_task_por_data

--- LARAVEL/to_do/app/Http/Controllers/HomeController.php
+++ LARAVEL/to_do/app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller {
     public function index(Request $request) {       // método index que retornará a view 'home'
 
-        $tasks = Task::all()->take(3);        // seleçionando 3 'tasks'
+        $tasks = Task::whereDate('due_date', date('Y-m-d'))-> get();        // seleçionando 4 'tasks'        'wherDate' procura por uma data espeçifica
         $AuthUser = Auth::user();       // incluidnso dados do usuário autenticado
         
         return view('home', ['tasks' => $tasks, 'AuthUser' => $AuthUser]);        // passando as 'tasks' e dados do usuário para view
