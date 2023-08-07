1e03f0772402435b1a610bc766db04a55f22be32
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-07 12:08:04 -0300

    criando_controller_'AuthMailController'_que_e_a_estrutura_necessaria_para_o_envio_de_email

---
+++ LARAVEL/envio_de_emails_e_processamento_de_filas/app/Http/Controllers/Mails/AuthMailController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Mails;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthMailController extends Controller
+{
+    public function sendRegisterMail() {        // método de envio de email
+
+    }
+}
---
+++ LARAVEL/envio_de_emails_e_processamento_de_filas/app/Mail/RegisterEmail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class RegisterEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function Build() {       // método 'Build' é responsavél por contruir a estrutura do email
+
+        return $this-> view('Mail.registerMail');       // o email retornará está view espeçifica
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Register Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
