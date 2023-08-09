ef82093bde410d172afbfe96f04b41a0cf04b07a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-09 00:49:53 -0300

    formatando_estilizacao_para_permitir_que_email_chegue_na_caixa_de_entrada_do_usuario_'spam_check'

--- LARAVEL/envio_de_emails_e_processamento_de_filas/resources/views/Mail/registerMail.blade.php
+++ LARAVEL/envio_de_emails_e_processamento_de_filas/resources/views/Mail/registerMail.blade.php
@@ -1,1 +1,16 @@
-<h1 style="color: blue;">Envio de 'E-mail' ultilizando Laravel, Programador se chama {{$nome}}</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <meta name="description" content="Descrição do meu email">
+</head>
+<body>
+    <div>
+        <h1 style="color: blue;">Envio de 'E-mail' ultilizando Laravel, Programador se chama {{$nome}}</h1>
+        <h1>Teste</h1>
+    </div>
+</body>
+</html>
