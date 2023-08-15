57e9c9c2fd5fff8ae2f9c20fa79a8484e9839043
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-15 19:13:35 -0300

    definindo_a_estrutura_basica_da_'home'_formatando_'sidebar'_e_'content'_ultilizando_'css

---
+++ LARAVEL/to_do/resources/views/layouts/home.blade.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            Logo
+        </div>
+        <div class="content">
+            <nav>       {{-- menu de navegação --}}
+                <a href="http://" class="btn btn-primary">
+                    Criar Tarefa
+                </a>
+            </nav>
+        </div>
+    </div>
+</body>
+</html>
