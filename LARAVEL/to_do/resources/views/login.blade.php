dff60ed0f1483e1f16d7f947bb0576e0aa78fa18
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-20 01:27:24 -0300

    passando_'propriedades'_para_os_componentes

---
+++ LARAVEL/to_do/resources/views/login.blade.php
@@ -0,0 +1,3 @@
+<x-layout page="To_do_App_Login">''{{-- 'page' alterará o titulo da tela --}}
+    Tela de login
+</x-layout>
--- LARAVEL/to_do/routes/web.php
+++ LARAVEL/to_do/routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/login', function () {      // rota 'login'
+    return view('login');
+});
