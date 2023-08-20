2ca082192df1994ffa6de9d60e91930b6324cdd3
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-20 01:17:27 -0300

    componetizando_'layout.blade.php'_e_o_enserindo-o_na_pagina_principal

---
+++ LARAVEL/to_do/resources/views/components/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoApp</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <img src="/assets/images/logo.jpg">
+        </div>
+        <div class="content">
+            <nav>       {{-- barra de de navegação --}}
+                <a href="http://" class="btn btn-primary">
+                    Criar Tarefa
+                </a>
+            </nav>
+            <main>      {{-- conteúdo prinçipal --}}
+                {{$slot}}
+            </main>
+        </div>
+    </div>
+</body>
+</html>
--- LARAVEL/to_do/routes/web.php
+++ LARAVEL/to_do/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('layouts/home');
+    return view('home');
 });
