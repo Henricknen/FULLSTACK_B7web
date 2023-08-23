27768692fa20d4164e1d6c38d838392ecb8b2147
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-23 00:48:00 -0300

    componetizando_'input'_do_formulario_para_ultilizar_em_outro_local_deixando_o_codigo_mais_legivel

--- LARAVEL/to_do/resources/views/tasks/create.blade.php
+++ LARAVEL/to_do/resources/views/tasks/create.blade.php
@@ -8,19 +8,17 @@
     <section id="create_task_section">
         <h1>Criar tarefa</h1>
         <form>
-            <div class="inputArea">
-                <label for="title">
-                    Título da task
-                </label>
-                <input id="title" name="title" placeholder="Digite o titulo da tarefa" required />
-            </div>
-
-            <div class="inputArea">
-                <label for="due_date">
-                    Data de realização
-                </label>
-                <input type="date" id="due_date" name="due_date" placeholder="Digite o titulo da tarefa" required />
-            </div>
+            <x-form.text_input     {{-- utilizando o componente 'text_input' --}}
+            name="title"
+            label="Título da Task" 
+            placeholder="Digite o titulo da sua Task" 
+            />
+            <x-form.text_input 
+            type="date" 
+            name="due_date" 
+            label="Data de realização" 
+            placeholder="Digite o titulo da tarefa" 
+            />
 
             <div class="inputArea">
                 <label for="category">
@@ -37,6 +35,9 @@
                     descrição da tarefa
                 </label>
                 <textarea name="description" placeholder="Digite uma descrição para sua task"></textarea>       {{-- textarea não é obrigado ser preenchido --}}
+            </div>
+            <div class="inputArea">
+                <button type="submit" class="btn btn-primary">Enviar</button>
             </div>
         </form>
     </section>
