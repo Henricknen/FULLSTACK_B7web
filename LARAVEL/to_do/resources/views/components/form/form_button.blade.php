fe4d8a1ea32fcd70ba271a2f2199c7c55ce9c54a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-24 00:12:16 -0300

    criando_componente_para_fazer_a_renderizacao_dos_dois_botoes

---
+++ LARAVEL/to_do/resources/views/components/form/form_button.blade.php
@@ -0,0 +1,4 @@
+<div class="inputArea">
+    <x-form.button type="submit">{{$submitTxt}}</x-form.button>     {{-- componente 'x-form.button' renderiza os dois botões reçebendo as variáveis do componente 'button.blade.php' --}}
+    <x-form.button type="reset">{{$resetTxt}}</x-form.button>
+</div>
