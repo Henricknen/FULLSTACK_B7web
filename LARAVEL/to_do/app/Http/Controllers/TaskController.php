8e663a56c0967f2a984db6020a2421c500a907fc
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-24 23:37:25 -0300

    sincronizando_'tasks'_com_o_banco_de_dados

--- LARAVEL/to_do/app/Http/Controllers/TaskController.php
+++ LARAVEL/to_do/app/Http/Controllers/TaskController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller {
@@ -10,15 +12,40 @@ class TaskController extends Controller {
     }
 
     public function create(Request $request)  {
-        return view('tasks.create');
+        $categories = Category::all();      // pegando todas as categorias
+        $data['categories'] = $categories;
+        return view('tasks.create', $data);     // passando '$data' com índice categories para a view
+    }
+
+    public function create_action(Request $request) {
+        $task = $request->only(['title', 'category_yd', 'description', 'due_date']);
+        $task['user_id'] = 1;
+
+
+        $request->validate([        // '$request->validate' Isso garantirá que os campos obrigatórios 'title' 'category_id' 'description' 'due_date' sejam fornecidos antes de inserir os dados na tabela tasks
+            'title' => 'required',
+            'category_id' => 'required',
+            'description' => 'required',
+            'due_date' => 'required',
+        ]);
+
+        // $taskData = [
+        //     'title' => $request->input('title'),
+        //     'category_id' => $request->input('category_id'),
+        //     'description' => $request->input('description'),
+        //     'due_date' => $request->input('due_date'),
+        //     'user_id' => 1, // definindo id aleatorio do usuario para testes
+        // ];
+
+        $dbTask = Task::create($task);        // Se desejar redirecionar para uma rota específica após a criação da tarefa, faça algo como:
+        return redirect()->route('home');
     }
 
     public function edit(Request $request)  {
         return view('tasks.edit');
     }
 
-    public function delete(Request $request)  {     // deleta e redireçiona para a 'home'
-        return redirect (route('home'));
-
+    public function delete(Request $request)  {        // e redirecionar para a 'home'
+        return redirect(route('home'));
     }
 }
