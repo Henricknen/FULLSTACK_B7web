0a2ca617f118f9da35195ea9fe58da06955d747d
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-26 23:52:34 -0300

    criando_tela_e_fazendo_o_registro_de_usuarios

--- LARAVEL/to_do/app/Http/Controllers/AuthController.php
+++ LARAVEL/to_do/app/Http/Controllers/AuthController.php
@@ -1,7 +1,9 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller {
@@ -11,5 +13,12 @@ class AuthController extends Controller {
 
     public function register(Request $request) {
         return view('register');
+    }
+
+    public function register_action(Request $request) {
+
+        $data = $request-> only('name', 'email', 'password');
+        $userCreated = User::create($data);
+        dd($userCreated);
     }
 }
--- LARAVEL/to_do/app/Http/Controllers/HomeController.php
+++ LARAVEL/to_do/app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use App\Models\Task;
 class HomeController extends Controller {
     public function index(Request $request) {       // método index que retornará a view 'home'
 
-        $tasks = Task::all()->take(4);        // seleçionando 3 'tasks'
+        $tasks = Task::all()->take(4); // seleçionando 4 'tasks'
 
         return view('home', ['tasks' => $tasks]);        // passando as 'tasks' pra view
     }
