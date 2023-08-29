657d32779aa607fcb22e5abe27af20e902deac6e
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-29 20:59:21 -0300

    ultilizando_'whereDate'_para_filtrar_task_por_data_atual_e_configurando_o_timezone_para_'America/Sao_Paulo'

--- LARAVEL/to_do/app/Http/Controllers/HomeController.php
+++ LARAVEL/to_do/app/Http/Controllers/HomeController.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller {
     public function index(Request $request) {       // método index que retornará a view 'home'
 
-        $tasks = Task::all()->take(4); // seleçionando 4 'tasks'
+        // $tasks = Task::all()->take(4); // seleçionando 4 'tasks'
+        $tasks = Task::whereDate('due_date', date('Y-m-d'))-> get(); // 'wherDate' seleçionando 'tasks' pela data atual
         $AuthUser = Auth::user();       // incluindo dados do usuário autenticado
 
         return view('home', ['tasks' => $tasks, 'AuthUser' => $AuthUser]);        // passando as 'tasks' e dados do usuário para view
