a2c4585dd1bb9297f3ee02c9c12767e7de8f31c0
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-08-29 21:25:02 -0300

    tornando_filtragem_de_datas_dinamica_via_'querystring'

--- LARAVEL/to_do/app/Http/Controllers/HomeController.php
+++ LARAVEL/to_do/app/Http/Controllers/HomeController.php
@@ -9,10 +9,29 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller {
     public function index(Request $request) {       // método index que retornará a view 'home'
 
-        // $tasks = Task::all()->take(4); // seleçionando 4 'tasks'
-        $tasks = Task::whereDate('due_date', date('Y-m-d'))-> get(); // 'wherDate' seleçionando 'tasks' pela data atual
-        $AuthUser = Auth::user();       // incluindo dados do usuário autenticado
+        // // $tasks = Task::all()->take(4); // seleçionando 4 'tasks'
+        // $tasks = Task::whereDate('due_date', date('Y-m-d'))-> get(); // 'wherDate' seleçionando 'tasks' pela data aty
+        // $AuthUser = Auth::user();       // incluindo dados do usuário autenticado
+        // return view('home', ['tasks' => $tasks, 'AuthUser' => $AuthUser]);        // passando as 'tasks' e dados do usuário para view
+        
+        if($request-> date) {       // se houver uma data filtrada
+            $filterDate = $request-> date;        // a variável '$filterDate' será definida como o resultado da filtragem
+        } else {        // se não
+            $filterDate = date('Y-m-d');      // '$filterDate' será a data atual
+        }
 
-        return view('home', ['tasks' => $tasks, 'AuthUser' => $AuthUser]);        // passando as 'tasks' e dados do usuário para view
+        // $data['date_as_string'] = '05 Agos';
+        // $data['date_prev_button'] = '2023-08-04';
+        // $data['date_next_button'] = '2023-08-06';
+
+        $data['tasks'] = Task::whereDate('due_date', $filterDate)-> get();        // 'wherDate' procura por uma data espeçifica e '$filterDate' filtra uma data
+
+        $data['AuthUser'] = Auth::user();       // incluidnso dados do usuário autenticado
+
+        $data['tasks_count'] = $data['tasks']-> count();
+
+        $data['undone_tasks_count'] = $data['tasks']-> where('is_done', false)-> count();
+
+        return view('home', $data);
     }
 }
