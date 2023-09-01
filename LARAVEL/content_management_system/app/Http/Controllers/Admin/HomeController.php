0ae9f034bdc51518e038a24374e19ae4717877b8
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-01 01:03:19 -0300

    configurando_o_basico_do_'AdminLTE'

--- LARAVEL/content_management_system/app/Http/Controllers/Admin/HomeController.php
+++ LARAVEL/content_management_system/app/Http/Controllers/Admin/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller {
 
     public function __construct() {
-        $this-> mmiddleware('auth');
+        // $this-> mmiddleware('auth');
     }
     
     public function index() {
--- LARAVEL/content_management_system/resources/views/admin/home.blade.php
+++ LARAVEL/content_management_system/resources/views/admin/home.blade.php
@@ -1,1 +1,21 @@
-Página iniçial do admin................
+@extends('adminlte::page');     {{-- extendedo 'adminlte' --}}
+
+@section('name')
+    
+@section('title', 'Painel')      {{-- alterando o titulo da página --}}
+
+@section('content_header')      {{-- tudo que estiver dentro desta tag aparecerá no iniçio da página --}}
+    <h1>Painel de Controle</h1>
+@endsection
+
+@section('content')     {{-- conteudo da página --}}
+    ....
+@endsection
+
+@section('css')     {{-- importações do 'css' --}}
+    <link rel="stylesheet" href="/assets/css/custom.css">
+@endsection
+
+@section('js')     {{-- inserindo javascript --}}
+    <script>alert("Rodando!");</script>
+@endsection
