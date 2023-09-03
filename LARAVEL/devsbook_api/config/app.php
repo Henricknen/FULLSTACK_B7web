2acdd123cc870da1c1d0f186fa62e8b0272348b5
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-03 13:28:18 -0300

    instalando_bibliotecas_'jwt_auth'_permite_a_autenticacao_baseada_em_tokens_jwt_e_a_'intervation_image'

--- LARAVEL/devsbook_api/config/app.php
+++ LARAVEL/devsbook_api/config/app.php
@@ -160,6 +160,8 @@ return [
          * Package Service Providers...
          */
 
+        Intervention\Image\ImageServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -183,6 +185,9 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+
+        'Image' => Intervention\Image\Facades\Image::class,
+
     ])->toArray(),
 
 ];
