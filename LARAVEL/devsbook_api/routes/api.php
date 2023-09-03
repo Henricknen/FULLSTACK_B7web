c466be37aaed8a109294d74d57b565c397eac6d4
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-03 19:04:53 -0300

    criando_as_rotas_que_sera_ultilidas_pela_'api'

--- LARAVEL/devsbook_api/routes/api.php
+++ LARAVEL/devsbook_api/routes/api.php
@@ -17,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {       // rota para teste, fazendo um endpoint 'ping'
     return ['pong'=> true];     // e retornando 'pong'
 });
+
+Route::get('/401', 'AuthController#unauthorizer')-> name('login');      // rota leva para página de erro se usúario não estiver logado
+
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/auth/logout', 'AuthController@logout');
+Route::post('/auth/refresh', 'AuthController@refresh');
+
+Route::post('/user', 'AuthController@create');
+Route::put('/user', 'UserController@update');
+Route::post('/user/avatar', 'UserController@updateAvatar');
+Route::post('/user/cover', 'UserController@updateCover');
+
+Route::get('/feed', 'FeedController@read');
+Route::get('/user/feed', 'FeedController@userFeed');
+Route::get('/user/{id}/feed', 'FeedController@userFeed');
+
+Route::get('/user', 'UserController@read');
+Route::get('/user/{id}', 'UserController@read');
+
+Route::post('/feed', 'FeedController@create');
+
+Route::post('/post/{id}/like', 'PostController@like');
+Route::post('/post/{id}/comment', 'PostController@comment');
+
+Route::post('/search', 'SearchController@search');
