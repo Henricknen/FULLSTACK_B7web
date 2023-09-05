0fc347506a546f2f27681f6c1c3d468e1ddfafe3
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-05 20:27:21 -0300

    criando_processo_de_'atualizacao'_de_informacoes_do_usuario

--- LARAVEL/devsbook_api/app/Http/Controllers/UserController.php
+++ LARAVEL/devsbook_api/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,5 +13,65 @@ class UserController extends Controller {
         $this-> middleware('auth:api');
 
         $this-> loggedUser = auth()-> user();
+    }
+
+    public function update(Request $request) {
+        $array = ['error'=> ''];
+
+        $name = $request-> input('name');       // pegando valores que serão enviados para criação do usuário
+        $email = $request-> input('email');
+        $birthdate = $request-> input('birthdate');
+        $city = $request-> input('city');
+        $work = $request-> input('work');
+        $password = $request-> input('password');
+        $password_confirm = $request-> input('password_confirm');
+
+        $user = User::find($this-> loggedUser['id']);      // pegando o usuário
+
+        if($name) {
+            $user-> name = $name;
+        }
+
+        if($email) {
+            if($email != $user-> email) {       // verificando se o email que usuário mandar é diferente do email atual
+                $emailExits = User::where('email', $email)-> count();
+                if($emailExits === 0) {
+                    $user-> email = $email;
+                } else {
+                    $arra['error'] = 'Email já existe...';
+                    return $array;
+                }
+            }
+        }
+
+        if($birthdate) {        // verificando se foi enviado alguma 'data de nasçimento'
+            if(strtotime($birthdate) === false) {
+                $array['error'] = 'Data de nasçimento invalida...';
+            }
+
+            $user-> birthdate = $birthdate;
+        }
+
+        if($city) {
+            $user-> city = $city;
+        }
+
+        if($work) {
+            $user-> work = $work;
+        }
+
+        if($password && $password_confirm) {
+            if($password === $password_confirm) {
+                $hash = paSsword_hash($password, PASSWORD_DEFAULT);     // gerando um hash da nova senha
+                $user-> password = $hash;
+            } else {
+                $array['error'] = 'As senhas nã batem...';
+                return $array;
+            }
+        }
+
+        $user-> save();
+
+        return $array;
     }
 }
--- LARAVEL/devsbook_api/routes/api.php
+++ LARAVEL/devsbook_api/routes/api.php
@@ -24,9 +24,8 @@ Route::post('/auth/login', 'AuthController@login');
 Route::post('/auth/logout', 'AuthController@logout');
 Route::post('/auth/refresh', 'AuthController@refresh');
 
-
 Route::post('/user', 'AuthController@create');
-// Route::put('/user', 'UserController@update');
+Route::put('/user', 'UserController@update');
 // Route::post('/user/avatar', 'UserController@updateAvatar');
 // Route::post('/user/cover', 'UserController@updateCover');
 
