dcfc1fd6498d215a074a86aae9c87639fcb0b00b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-09 14:35:00 -0300

    criando_os_'controllers'_e_declarando_os_dentro_de_'api.php'_para_serem_ultilizados_quando_se_fazer_as_rotas

--- LARAVEL/get_cond/app/Models/Reservation.php
+++ LARAVEL/get_cond/app/Models/Reservation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Reservation extends {
+class Reservation extends Model {
     use HasFactory;
 
     public $timestamps = false;
--- LARAVEL/get_cond/routes/api.php
+++ LARAVEL/get_cond/routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Controllers\AuthController;
+use App\Controllers\BilletController;
+use App\Controllers\DocController;
+use App\Controllers\FoundAndLostController;
+use App\Controllers\ReservationController;
+use App\Controllers\UnitController;
+use App\Controllers\UserController;
+use App\Controllers\WallController;
+use App\Controllers\WarningController;
+
 Route::get('/ping', function() {
     return ['pong'=> true];
 });
