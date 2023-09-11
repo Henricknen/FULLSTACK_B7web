6e0af73e62cc5299aea2b0d70efacfd95fb7181f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-11 15:53:45 -0300

    criando_um_'seeder'_para_preencher_o_banco_de_dados_com_alguns_dados_padrao

--- LARAVEL/get_cond/database/seeders/DatabaseSeeder.php
+++ LARAVEL/get_cond/database/seeders/DatabaseSeeder.php
@@ -2,21 +2,72 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {        
+        DB::table('units')->insert([
+            'name' => 'Apartamento 100',
+            'id_owner' => 1,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('units')->insert([
+            'name' => 'Apartamento 101',
+            'id_owner' => 1,
+        ]);
+
+        DB::table('units')->insert([
+            'name' => 'Apartamento 300',
+            'id_owner' => 0,
+        ]);
+
+        DB::table('units')->insert([
+            'name' => 'Apartamento 250',
+            'id_owner' => 0,
+        ]);
+
+        DB::table('areas')->insert([
+            'allowed' => 1,
+            'title' => 'Academia',
+            'cover' => 'gym.jpg',
+            'days' => '1, 2, 3, 4',
+            'start_time' => '06:00:00',
+            'end_time' => '22:00:00',
+        ]);
+
+        DB::table('areas')->insert([
+            'allowed' => 1,
+            'title' => 'Piscina',
+            'cover' => 'pool.jpg',
+            'days' => '1, 2, 3, 4, 5',
+            'start_time' => '07:00:00',
+            'end_time' => '23:00:00',
+        ]);
+
+        DB::table('areas')->insert([
+            'allowed' => 1,
+            'cover' => 'barbecue.jpg',
+            'title' => 'Churrasqueira',
+            'days' => '4, 5, 6',
+            'start_time' => '09:00:00',
+            'end_time' => '23:00:00',
+        ]);
+
+        DB::table('walls')->insert([
+            'title' => 'Título de aviso',
+            'body' => 'saffdsdsghgfjhj hkghkgkg jjllklkhlkl',
+            'datecreated' => '2023-09-11 15:32:25',
+        ]);
+
+        DB::table('walls')->insert([
+            'title' => 'Alerta geral para todos',
+            'body' => '.....................Danger....................',
+            'datecreated' => '2023-09-11 18:00:00',
+        ]);
     }
 }
