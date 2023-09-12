e03b0e669b357ef7eb5db6b6d1ad7f9647789f89
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-12 17:57:27 -0300

    criacao_do_processo_de_'leitura'_dos_documentos

--- LARAVEL/get_cond/app/Http/Controllers/DocController.php
+++ LARAVEL/get_cond/app/Http/Controllers/DocController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-class DocController extends Controller
-{
-    //
+use App\Models\Doc;
+
+class DocController extends Controller {
+    public function getAll() {
+        $array = ['error' => ''];
+
+        $docs = Doc::all();
+
+        $array['list'] = $docs;     // adiçionando todos 'all' os documentos na lista
+
+        foreach($docs as $docKey=> $docValue) {
+            $docs[$docKey]['fileurl'] = asset('storage/'. $docValue['fileurl']);        // mudando a 'url' do arquivo
+        }
+
+        return $array;
+    }
 }
