52873e6a25b93e08f49b8474e995abd2fbe7641d
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-11 23:50:40 -0300

    criando_a_leitura_do_mural_de_avisos_'wall'

--- LARAVEL/get_cond/app/Http/Controllers/WallController.php
+++ LARAVEL/get_cond/app/Http/Controllers/WallController.php
@@ -4,7 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WallController extends Controller
-{
-    //
+use App\Models\User;
+use App\Models\Wall;
+use App\Models\WallLike;
+
+class WallController extends Controller {
+    public function getAll() {
+        $array = ['error'=> '', 'list'=> []];       // 'list' retorna uma lista vazia ou com os itens caso tenha-os
+
+        $user = auth()-> user();        // pegando o usuário logado
+
+        $walls = Wall::all();        // pegando todos avisos do mural
+
+        foreach($walls as $wallKey=> $wallValue) {      // complementando as informações de 'wall'
+            $walls[$wallKey]['liked'] = 0;
+            $walls[$wallKey]['liked'] = false;
+
+            $likes = WallLike::where('id_wall', $wallValue['id'])-> count();        // pegando quantidade total de 'likes'
+            $walls[$wallKey]['liked'] = $likes;     // atualizando a quantidade de 'likes'
+
+            $melikes = WallLike::where('id_wall', $wallValue['id'])     // procura na tabela 'WllLikes' algum registro em que eu dei um 'like'
+            ->where('id_user', $user['id'])
+            ->count();
+
+            if($melikes > 0) {
+                $walls[$wallKey]['liked'] = true;
+            }
+        }
+
+        $array['list'] = $walls;
+
+        return $array;
+    }
 }
