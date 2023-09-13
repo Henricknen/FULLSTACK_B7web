5e2a6e5f1b319adb0d3e2afb1c898623c0c60763
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-13 00:07:08 -0300

    processo_de_boletos_'billets'

--- LARAVEL/integracoes/app/Http/Controllers/BilletController.php
+++ LARAVEL/integracoes/app/Http/Controllers/BilletController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-class BilletController extends Controller
-{
-    //
+use App\Models\Billet;
+use App\Models\Unit;
+
+class BilletController extends Controller {
+    public function getAll(Request $request) {
+        $array = ['error' => ''];
+
+        $property = $request-> input('property');       // pegando a propriedade
+        if($property) {
+            $user = auth()-> user();        // pegando o usuário logado
+
+            $unit = Unit::where('id', $property)
+            -> where ('id_owner', $user['id'])
+            -> count();
+
+            if($unit > 0) {
+                $billets = Billet::where('id_unit', $property)-> get();     // pegando boletos do usuario logado 
+
+                foreach($billets as $billetKey=> $billetValue) {
+                    $billets[$billetKey]['fileurl'] = asset('storage/'. $billetValue['fileurl']);
+                }
+
+                $array['list'] = $billets;
+            } else {
+                $array['error'] = 'Está unidade não te pertençe...';
+            }
+
+        } else {
+            $array['error'] = 'A propriedade é obrigatoria...';
+        }
+
+        return $array;
+    }
 }
--- LARAVEL/integracoes/app/Http/Controllers/WarningController.php
+++ LARAVEL/integracoes/app/Http/Controllers/WarningController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WarningController extends Controller
-{
+class WarningController extends Controller {
     //
 }
