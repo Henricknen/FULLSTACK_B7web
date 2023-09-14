53e00b9741766be7b5f39b59e31059c9f365810a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-14 19:07:32 -0300

    criacao_do_'foundlostcontroller'_achados_e_perdidos

--- LARAVEL/integracoes/app/Http/Controllers/FoundAndLostController.php
+++ LARAVEL/integracoes/app/Http/Controllers/FoundAndLostController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
 
-class FoundAndLostController extends Controller
-{
-    //
+class FoundAndLostController extends Controller {
+    public function getAll(Request $request) {
+        $array = ['error'=> ''];
+        
+        $lost = FoundAndLost::where('status', 'LOST')       // pegando a lista dos 'perdidos'
+        -> orderBy('datecreated', 'DESC')
+        -> orderBy('id', 'DESC')
+        -> get();
+        
+        $recovered = FoundAndLost::where('status', 'RECOVERED')     // pegando a lista dos 'achados'
+        -> orderBy('datecreated', 'DESC')
+        -> orderBy('id', 'DESC')
+        -> get();
+
+        foreach($lost as $lostKey=> $lostValue) {
+            $lost[$lostKey]['datecreated'] = date('d/m/Y', strtotime($lostValue['datecreated']));       // corrigindo a data
+            $lost[$lostKey]['photo'] = asset('storage/'. $lostValue['photo']);       // corrigindo a foto
+        }
+
+        foreach($recovered as $recKey=> $recValue) { 
+            $recovered[$recKey]['datecreated'] = date('d/m/Y', strtotime($recValue['datecreated']));
+            $recovered[$recKey]['photo'] = asset('storage/'. $recValue['photo']);
+        }
+
+        $array['lost'] = $lost;     // jogando itens perdidos dentro do array 'lost'
+        $array['recovered'] = $recovered;     // jogando itens achados dentro do array 'recovered'
+        
+        return $array;
+    }
 }
