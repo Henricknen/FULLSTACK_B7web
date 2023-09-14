c4b2ee25dd824f20f0ad6342b7504ba9d3fa2470
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-14 20:50:27 -0300

    criacao_do_'insert'_que_insere_intens_na_lista_de_achados_e_perdidos

--- LARAVEL/integracoes/app/Http/Controllers/FoundAndLostController.php
+++ LARAVEL/integracoes/app/Http/Controllers/FoundAndLostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\FoundAndLost;
 
 class FoundAndLostController extends Controller {
     public function getAll(Request $request) {
@@ -31,6 +34,38 @@ class FoundAndLostController extends Controller {
 
         $array['lost'] = $lost;     // jogando itens perdidos dentro do array 'lost'
         $array['recovered'] = $recovered;     // jogando itens achados dentro do array 'recovered'
+        
+        return $array;
+    }
+
+    public function insert(Request $request) {      // inserindo regitro na lista
+        $array = ['error'=> ''];
+        
+        $validator = Validator::make($request-> all(), [        // criando regras de validação
+            'description'=> 'required',
+            'where'=> 'required',
+            'photo'=> 'required|file\mimes: jpg, npg'
+        ]);
+
+        if(!$validator-> fails()) {     // verificando se deu tudo certo na validação
+            $description = $request-> input('descripition');        // pegando os itens
+            $where = $request-> input('where');
+            $file = $request-> file('photo')-> store('public');     // salvando 'photo'
+            $file = explode('public/', $file);
+            $photo = $file[1];
+
+            $newLost = new FoundAndLost();      // enserindo os regitros no banco de dados
+            $newLost-> status = 'LOST';
+            $newLost-> photo = $photo;
+            $newLost-> description = $description;
+            $newLost-> where = $where;
+            $newLost-> datecreated = date('Y-m-d');
+            $newLost-> save();
+            
+        } else {
+            $array['error'] = $validator-> errors()-> first();
+            return $array;
+        }
         
         return $array;
     }
