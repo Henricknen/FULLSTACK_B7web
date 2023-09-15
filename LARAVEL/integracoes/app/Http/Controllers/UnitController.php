55ba186c51e76698de8a9256af1ad773a6c01b24
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-15 14:03:51 -0300

    processo_de_'unit'_das_unidade

--- LARAVEL/integracoes/app/Http/Controllers/UnitController.php
+++ LARAVEL/integracoes/app/Http/Controllers/UnitController.php
@@ -4,7 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UnitController extends Controller
-{
-    //
+use App\Models\Unit;
+use App\Models\UnitPeople;
+use App\Models\UnitVehicles;
+use App\Models\UnitPet;
+
+class UnitController extends Controller {
+    public function getInfo($id) {      // rotas das unidades reçeberá um 'id'
+        $array = ['error' => ''];
+
+        $unit = Unit::find($id); // verificando se 'id' é de uma unidade existente
+        if ($unit) {
+
+            $peoples = UnitPeople::where('id_unit', $id)->get();     // pegando informações de 'people', 'vehicles' e 'pets'
+            $vehicles = UnitVehicles::where('id_unit', $id)->get();
+            $pets = UnitPet::where('id_unit', $id)->get();
+
+            foreach($peoples as $pKey=> $pValue) {
+                $poples[$pKey]['birthdate'] = date('d/m/Y', strtotime($pValue['birthdate']));       // correção da data de nasçimento
+            }
+
+            $array['peoples'] = $peoples;       // jogando as informações dentro dos arrays correpondentes
+            $array['vehicles'] = $vehicles;
+            $array['pets'] = $pets;
+
+        } else {
+            $array['error'] = 'Propriedade inexistente';
+            return $array;
+        }
+
+        return $array;
+    }
 }
