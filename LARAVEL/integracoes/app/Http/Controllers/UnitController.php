d05c5d8d833203b77647a43fdded923b38942114
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-15 19:47:06 -0300

    processo_de_de_adicionar_'peoples'_'vehicles'_e_'pets'_na_unidade

--- LARAVEL/integracoes/app/Http/Controllers/UnitController.php
+++ LARAVEL/integracoes/app/Http/Controllers/UnitController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Unit;
 use App\Models\UnitPeople;
@@ -33,6 +34,84 @@ class UnitController extends Controller {
             return $array;
         }
 
+        return $array;
+    }
+
+    public function addPerson($id, Request $request) {
+        $array = ['error'=> ''];
+        
+        $validator = Validator::make($request-> all(), [
+            'name' => 'required',
+            'birthdate' => 'required|date'
+        ]);
+        if($validator-> fails()) {
+            $name = $request-> input('name');
+            $birthdate = $request-> input('birthdate');
+
+            $newPerson = new UnitPeople();
+            $newPerson-> id_unit = $id;
+            $newPerson-> name = $name;
+            $newPerson-> birthdate = $birthdate;
+            $newPerson-> save();
+
+        } else {
+            $array['error'] = $validator-> errors()-> first();
+            return $array;
+        }
+        
+        return $array;
+    }
+
+    public function addVehicle($id, Request $request) {
+        $array = ['error'=> ''];
+        
+        $validator = Validator::make($request-> all(), [
+            'title' => 'required',
+            'color' => 'required',
+            'plate' => 'required'
+        ]);
+        if($validator-> fails()) {
+            $title = $request-> input('title');
+            $color = $request-> input('color');
+            $plate = $request-> input('plate');
+
+            $newVehicle = new UnitVehicles();
+            $newVehicle-> id_unit = $id;
+            $newVehicle-> title = $title;
+            $newVehicle-> color = $color;
+            $newVehicle-> plate = $plate;
+            $newVehicle-> save();
+
+        } else {
+            $array['error'] = $validator-> errors()-> first();
+            return $array;
+        }
+        
+        return $array;
+    }
+
+    public function addPet($id, Request $request) {
+        $array = ['error'=> ''];
+        
+        $validator = Validator::make($request-> all(), [
+            'name' => 'required',
+            'race' => 'required'
+        ]);
+        if($validator-> fails()) {
+            $name = $request-> input('name');
+            $race = $request-> input('race');
+
+            $newPerson = new UnitPeople();
+            $newPerson-> id_unit = $id;
+            $newPerson-> name = $name;
+            $race-> birthdate = $race;
+            $newPerson-> save();
+
+        } else {
+            $array['error'] = $validator-> errors()-> first();
+            return $array;
+        }
+        
         return $array;
     }
 }
