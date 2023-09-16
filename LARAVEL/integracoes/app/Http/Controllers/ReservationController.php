96bab00c5f51bfa8744ab5fb25fc0f59ce43f3e1
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-16 17:25:20 -0300

    criando_processo_'getReservations'_que_pega_as_reservas_disponiveis

--- LARAVEL/integracoes/app/Http/Controllers/ReservationController.php
+++ LARAVEL/integracoes/app/Http/Controllers/ReservationController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Area;
 use Illuminate\Http\Request;
 
-class ReservationController extends Controller
-{
-    //
+class ReservationController extends Controller {
+    public function getReservations(Request $request) {     // pega as áreas disponíveis
+        $array = ['error'=> '', 'list'=> []];
+        $daysHelper = ['Dom', 'Seg', 'Ter', 'Quar'. 'Qui', 'Sex', 'Sáb'];
+        
+        $areas = Area::where('allowed', 1)-> get();     // pegando as áreas que são permitidas fazerem reservas
+
+        foreach($areas as $area) {
+            $dayList = explode(',', $area['days']);
+
+            $dayGroups = [];        // array 'dayGroups' é o que vai ficar com o grupo de dias
+
+            //      adiçionando o primerio dia
+            $lastDay = intval(current($dayList));
+            $dayGroups[] = $daysHelper[$lastDay];
+            array_shift($dayList);      // remoção do primeiro item da lista
+
+            // adiçionando dias relevantes
+            foreach($dayList as $day) {
+                if(intval($day) != $lastDay + 1) {
+                    $dayGroups = $daysHelper[$lastDay];
+                    $dayGroups = $daysHelper[$day];
+                }
+            }
+
+            //      adiçionando o último dia
+            $dayGroups[] = $daysHelper[end($dayList)];
+
+            //      juntando as datas
+            $dates = '';
+            $close = 0;
+            foreach($dayGroups as $group) {
+                if($close === 0) {
+                    $dates .= $group;
+                } else {
+                    $dates .= '-'. $group. ',';
+                }
+                $close = 1 - $close;
+            }
+
+            $dates = explode(',', $dates);
+            array_pop($dates);     // removendo a virgula do final
+
+            //      adiçionando o 'time'
+            $start = date('H:i', strtotime($area['start-time']));
+            $end = date('H:i', strtotime($area['end-time']));
+
+            foreach($dates as $dKey=> $dValue) {
+                $dates[$dKey] .= ' '. $start. 'as'. $end;
+            }
+            
+            $arry['list'][] = [
+                'id'=> $area['id'],
+                'cover'=> asset('storage/'. $area['cover']),
+                'title'=> $area['title'],
+                'dates'=> 'dates'
+            ];
+        }
+        
+        return $array;
+    }
 }
