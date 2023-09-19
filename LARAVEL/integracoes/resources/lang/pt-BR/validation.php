91229b909d0538d15b451ec357f847f7ada4b15a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-19 19:50:06 -0300

    criando arquivo 'validation.php'_para_fazer_logica_de_traducao_das_menssagens_de_erros

---
+++ LARAVEL/integracoes/resources/lang/pt-BR/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'welcome' => 'Bem-vindo a -api- intregacoes Laravel',    
+    'password' => 'Senha',
+    'required' => 'O campo :attribute é obrigatorio.',
+    'email' => 'O :attribute Precisa ser um e-mail válido.',
+    'unique' => 'O :attribute já foi ultilizado.',
+    'digits' => 'O campo :attribute preçisar ter :digits dígitos.',
+    'same' => 'O campo :attribute e :other precisam bater.',
+    'file' => 'O campo :attribute deve ser um arquivo.',
+    'mimes' => 'O campo :attribute deve ser um arquivo do tipo: :values.',
+    'date_format' => 'O :attribute não corresponde ao formato: :format.',
+    'date' => 'O :attribute não é uma data válida.',
+];
