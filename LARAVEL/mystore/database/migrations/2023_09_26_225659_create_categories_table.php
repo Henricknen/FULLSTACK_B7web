f9bae26087575b2687a93c1b9502307300eeabee
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-09-26 23:16:04 -0300

    criando_'migrations'_categories_e_states

---
+++ LARAVEL/mystore/database/migrations/2023_09_26_225659_create_categories_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('categories', function (Blueprint $table) {      // criação da tabela 'categories' com dois campos
+            $table-> id();       // campo 'id'
+            $table-> string('name')-> unique();     // campo 'name' unico
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('categories');
+    }
+};
