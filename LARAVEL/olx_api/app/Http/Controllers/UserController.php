2633ab5eb163313c366a53f6aec418f2b4cb02c3
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-05 23:41:55 -0300

    criando_um_'hash'_da_senha_e_gerando_o_'token'_do_usuario

--- LARAVEL/olx_api/app/Http/Controllers/UserController.php
+++ LARAVEL/olx_api/app/Http/Controllers/UserController.php
@@ -6,16 +6,18 @@ use App\Http\Requests\CreateUserRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller {
     public function signup(CreateUserRequest $request): JsonResponse {      // ultilizando a request 'CreateUserRequest'
 
         $data = $request-> only(['name', 'email', 'password', 'state_id']);     // registrando o usuário no banco de dados
+        $data['password'] = Hash::make($data['password']);      // gerando um 'hash' da senha
         $user = User::create($data);
 
         $response = [
             'error'=> '',
-            'user'=> $user
+            'token'=> $user-> createToken('Register_token')-> plainTextToken        // criando o token de autenticação do usuário
         ];
 
         return response()-> json($response);
