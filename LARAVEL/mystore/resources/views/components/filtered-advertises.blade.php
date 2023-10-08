7a5106aa558d6eaae04794b0b3764f748caa607b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-07 23:38:47 -0300

    criando_e_componetizando_o_'advertlist'_area_de_anuncios_e_fazendo_a_subcomponetizacao_dos_anuncios

---
+++ LARAVEL/mystore/resources/views/components/filtered-advertises.blade.php
@@ -0,0 +1,99 @@
+<div class="categories-area">
+    <div class="title">Categorias</div>
+    <div class="buttons">
+      <button class="cars">
+        <img src="assets/icons/carIcon.png" alt="Ícone Carros" />
+        Carros
+      </button>
+      <button class="eletronics">
+        <img
+          src="assets/icons/eletronicsIcon.png"
+          alt="Ícone Eletrônicos"
+        />
+        Eletrônicos
+      </button>
+      <button class="clothes">
+        <img src="assets/icons/clothesIcon.png" alt="Ícone Roupas" />
+        Roupas
+      </button>
+      <button class="sports">
+        <img src="assets/icons/sportsIcon.png" alt="Ícone Esportes" />
+        Esportes
+      </button>
+      <button class="babies">
+        <img src="assets/icons/babiesIcon.png" alt="Ícone Bebês" />
+        Bebês
+      </button>
+    </div>
+  </div>
+  <div class="ads">
+    <div class="ads-title">Anúncios recentes</div>
+    <div class="ads-area">
+      <x-simpleadivertises
+      
+      bgImage = "http://placeholder.it/140x140"
+      title = "Bola!! de Futebol Americano Wilson" 
+      $price = "R$ 138,61"
+      $href = "#"
+      
+      />
+      {{-- <x-simple-adivertises /> --}}
+      {{-- <x-simple-adivertise /> --}}
+      <div class="ad-item">
+        <div
+          class="ad-image"
+          style="background-image: url('http://placehold.it/150x150')"
+        ></div>
+        <div class="ad-title">Tênis vans Baby - 1 ano</div>
+        <div class="ad-price">R$ 189,99</div>
+      </div>
+      <div class="ad-item">
+        <div
+          class="ad-image"
+          style="background-image: url('http://placehold.it/150x150')"
+        ></div>
+        <div class="ad-title">Controle PS4 - Preto</div>
+        <div class="ad-price">R$ 275,00</div>
+      </div>
+      <div class="ad-item">
+        <div
+          class="ad-image"
+          style="background-image: url('http://placehold.it/150x150')"
+        ></div>
+        <div class="ad-title">Volkswagen Fusca 68</div>
+        <div class="ad-price">R$ 34.990,00</div>
+      </div>
+      <div class="ad-item">
+        <div
+          class="ad-image"
+          style="background-image: url('http://placehold.it/150x150')"
+        ></div>
+        <div class="ad-title">Volkswagen Polo 2015 - Azul</div>
+        <div class="ad-price">R$ 67.900,00</div>
+      </div>
+      <div class="ad-item">
+        <div
+          class="ad-image"
+          style="background-image: url('http://placehold.it/150x150')"
+        ></div>
+        <div class="ad-title">Camisas sociais masculinas</div>
+        <div class="ad-price">R$ 110,00</div>
+      </div>
+      <div class="ad-item">
+        <div
+          class="ad-image"
+          style="background-image: url('http://placehold.it/150x150')"
+        ></div>
+        <div class="ad-title">Bola de Basquete Spalding - NBA</div>
+        <div class="ad-price">R$ 190,36</div>
+      </div>
+      <div class="ad-item">
+        <div
+          class="ad-image"
+          style="background-image: url('http://placehold.it/150x150')"
+        ></div>
+        <div class="ad-title">MacBook Pro</div>
+        <div class="ad-price">R$ 8.349,10</div>
+      </div>
+    </div>
+  </div>
---
+++ LARAVEL/mystore/resources/views/components/simple-advertises.blade.php
@@ -0,0 +1,5 @@
+<a href="{{$href}}" class="ad-item">
+    <div class="ad-image" style="background-image: url('{{$bgImage}}"></div>
+    <div class="ad-title">{{$title}}</div>
+    <div class="ad-price">{{$price}}</div>
+</a>
