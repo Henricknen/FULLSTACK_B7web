8188cdbda63cf6e773ad85dbc03ac49a2f0bdb32
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-10 21:29:33 -0300

    utilizando_loop_'while'_que_repetira_ate_que_a_condicao_seja_atendida

---
+++ PHP_ZP/php_intermediario/while.php
@@ -0,0 +1,9 @@
+<?php
+$x = 0;     // iniçializando a variável '$x'
+
+while($x < 10) {        // condição de parada
+    echo "X é igual a: ". $x. "<br/>";
+    $x++;       // incrementando o valor da variável '$x' a cada interação
+}
+
+?>
