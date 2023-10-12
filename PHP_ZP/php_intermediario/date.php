cb9c4c1bf9f087d75f68ea933f6b9aa198287d21
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-12 00:42:26 -0300

    ultilizando_funcao_'date'_que_faz_a_formatacao_da_data_e_hora_local

---
+++ PHP_ZP/php_intermediario/date.php
@@ -0,0 +1,7 @@
+<?php
+
+$dataAtual = date("d/m/Y \à\s H:i:s");     // retornando 'dia, mes, ano, horas, minutos e segundos' com string e '\' dentro do parâmetro indica para o php que é um texto
+
+echo $dataAtual;
+
+?>
