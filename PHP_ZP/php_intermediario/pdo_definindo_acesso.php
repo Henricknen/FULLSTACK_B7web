d56f849b4c54674e9ebf0d8c54400f632d485376
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-14 19:04:20 -0300

    criando_'variavel_de_conexao'_com_banco_de_dados_ultilizando_'pdo'_que_e_uma_biblioteca_'especializada_em_conexao'_com_bd

---
+++ PHP_ZP/php_intermediario/pdo_definindo_acesso.php
@@ -0,0 +1,7 @@
+<?php
+
+$string_de_conexao = "mysql:dbname=blog;host=localhost";      // variável de conexão ultilizando o nome do 'tipo' do banco de dados, o 'nome' do banco de dados e o host 'ip'  do banco de dados com parâmetro
+$dbuser = "root";       // usuário do banco de dados
+$dbpassword = "";     // senha do banco de dados
+
+?>
