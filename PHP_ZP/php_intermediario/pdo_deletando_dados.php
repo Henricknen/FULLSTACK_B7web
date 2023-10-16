73de7ebb3a3c7538d1abe0b5fe5b3007b61eb900
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-16 00:18:56 -0300

    removendo_dados_do_banco_de_dados_ultilizando_'delete'

---
+++ PHP_ZP/php_intermediario/pdo_deletando_dados.php
@@ -0,0 +1,17 @@
+<?php
+
+$string_de_conexao = "mysql:dbname=blog;host=localhost";      // variável de conexão ultilizando o nome do 'tipo' do banco de dados, o 'nome' do banco de dados e o host 'ip'  do banco de dados com parâmetro
+$dbuser = "root";       // usuário do banco de dados
+$dbpass = "";     // senha do banco de dados
+
+try {
+    $pdo = new PDO($string_de_conexao, $dbuser, $dbpass);       // iniçiando a biblioteca 'PDO' que se conectará com o banco de dados
+ 
+    $sql = "DELETE FROM usuarios WHERE id = 3";        // deletando 'quando' id for igual à '3'
+    $pdo-> query($sql);
+
+    echo "Usuário deletado com sucesso...";
+
+} catch(PDOException $e) {     // 'catch' executa as menssagens de erro 'PDOException' espeçifica a variável que será madado o erro
+    echo "Falha: ". $e-> getMessage();
+}
