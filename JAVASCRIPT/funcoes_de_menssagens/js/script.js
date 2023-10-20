07014f2f0b667fa8406c1d47faae2f6754238cf4
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-20 01:27:43 -0300

    ultilizando_funcoes_'alert'_que_fornece_informacoes_ou_solicita_uma_confirmacao_e_'console.log'_que_imprime_menssagens_ou_valores_no_console_do_navegador

---
+++ JAVASCRIPT/funcoes_de_menssagens/js/script.js
@@ -0,0 +1,3 @@
+alert("menssagem 'alert'")      // 'alert' exibe uma caiza de diálogo com uma menssagem espeçificada quando a página é quando a página é atualizada
+
+console.log("Luis Henrique Silva Ferreira")
