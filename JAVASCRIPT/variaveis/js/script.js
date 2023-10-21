4b010fca823fcc42aa1adeda7de5322680064488
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-20 23:41:21 -0300

    ultilizando_'let'_para_criar_'variaveis'_que_e_local_onde_se_armazena_algum_valor_especifico

---
+++ JAVASCRIPT/variaveis/js/script.js
@@ -0,0 +1,7 @@
+let graduacao = "Analise e desenvolvimento de sistemas"     // variáveis
+let tecnico = "Informatica para internet"
+let tecnicoo = "Eletroeletronica"
+
+console.log(graduacao)     // exibindo informações no 'console'
+console.log(tecnico)
+console.log(tecnicoo)
