36e7d1ae25baa5012f4a9f3368e045ce3bbe76c4
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-23 00:23:47 -0300

    criando_botoes_para_mudar_as_ordens_enviando_o_formulario_para_si_proprio_e_ordenando_os_nomes_em_ordem_'ascendente'_crecente

--- PHP_ZP/php_intermediario/projeto_ordenar/index.php
+++ PHP_ZP/php_intermediario/projeto_ordenar/index.php
@@ -9,13 +9,21 @@ try {
 
 ?>
 
+<form method = "GET">       <!-- 'form' sem action envia os dados para está própria página -->
+    <select name = "ordem" onchange = "this.form.submit()">       <!-- ao fazer alguma coisa 'onchange', envia 'this.form.submit' o próprio formúlario -->
+        <option></option>
+        <option value = "nome">Pelo nome</option>
+        <option value = "idade">Pelo idade</option>
+    </select>
+</form>
+
 <table border="1" width="400">      <!-- tabela mostrará os nome e idade dos usuários do banco de dados -->
     <tr>
         <th>Nome</th>
         <th>Idade</th>
     </tr>
     <?php
-    $sql = "SELECT * FROM usuarios";        // consulta
+    $sql = "SELECT * FROM usuarios ORDER BY nome ASC";        // consulta 'ordenada' pelo nome
     $sql = $pdo-> query($sql);
     if($sql-> rowCount() > 0) {
         foreach($sql-> fetchAll() as $usuario):
