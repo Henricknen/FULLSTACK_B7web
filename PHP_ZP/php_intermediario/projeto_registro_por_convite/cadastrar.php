f1f9984a2af3bf6a7906d22363848c3aabf14d57
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-24 23:37:26 -0300

    fazendo_alteracoes_no_projeto_de_'registro_por_convite'

--- PHP_ZP/php_intermediario/projeto_registro_por_convite/cadastrar.php
+++ PHP_ZP/php_intermediario/projeto_registro_por_convite/cadastrar.php
@@ -1,50 +1,50 @@
 <?php
-
 session_start();
 require 'config.php';
 
-if(!empty($_GET['codigo'])) {       // verificando se foi reçebido o 'codigo'
-    $codigo = addslashes($_GET['codigo']);
+if(!empty($_GET['codigo'])) {       // verificando se sistema enviou o 'codigo'
+	$codigo = addslashes($_GET['codigo']);
 
-    $sql = "SELECT * FROM usuarios WHERE codigo = '$codigo";        // verificando se 'código' existe, está salvo no banco de dados
-    $sql = $pdo-> query($sql);
+	$sql = "SELECT * FROM usuarios WHERE codigo = '$codigo'";        // verificando se 'código' existe, está salvo no banco de dados
+	$sql = $pdo->query($sql);
 
-    if($sql-> rowCount() == 0) {
-        header("Location: login.php");
-        exit;    
-    }
+	if($sql->rowCount() == 0) {        // se mandar código errado o usuário vai para 'login.php'
+		header("Location: login.php");
+		exit;
+	}
 } else {
-    header("Location: login.php");
-    exit;
+	header("Location: login.php");      // se nao mandar o código o usuário vai para login.php
+	exit;
 }
 
 if(!empty($_POST['email'])) {
-    $email = addslashes($_POST['email']);
-    $senha = addslashes($_POST['senha']);
+	$email = addslashes($_POST['email']);
+	$senha = md5($_POST['senha']);
 
-    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
-    $sql = $pdo-> query($sql);
+	$sql = "SELECT * FROM usuarios WHERE email = '$email'";
+	$sql = $pdo->query($sql);
 
-    if($sql-> rowCount() <= 0) {
+	if($sql->rowCount() <= 0) {
 
-        $codigo = md5(rand(0, 99999). rand(0, 99999));
-        $sql = "INSERT INTO usuarios (email, senha, codigo) VALUES ('$email', '$senha', '$codigo')";
-        $sql = $pdo-> query($sql);
+		$codigo = md5(rand(0,99999).rand(0,99999));
+		$sql = "INSERT INTO usuarios (email, senha, codigo) VALUES ('$email', '$senha', '$codigo')";
+		$sql = $pdo->query($sql);
 
-        unset($_SESSION['logado']);
+		unset($_SESSION['logado']);
 
-        header("Location: index.php");
-        exit;
-    }
+		header("Location: index.php");
+		exit;
+	}
 }
 ?>
-
 <h3>Cadastrar</h3>
 
 <form method="POST">
-    E-mail:<br/>
-    <input type="email" name="email" /><br/><br/>
-    
-    Senha:<br/>
-    <input type="password" name="senha" /><br/><br/>
+	E-mail:<br/>
+	<input type="email" name="email" /><br/><br/>
+
+	Senha:<br/>
+	<input type="password" name="senha" /><br/><br/>
+
+	<input type="submit" value="Cadastrar" />
 </form>
--- PHP_ZP/php_intermediario/projeto_registro_por_convite/index.php
+++ PHP_ZP/php_intermediario/projeto_registro_por_convite/index.php
@@ -3,20 +3,21 @@ session_start();        // verificando se usuário está logado
 require 'config.php';
 
 if(empty($_SESSION['logado'])) {
-    header("Location: login.php");
-    exit;
+	header("Location: login.php");
+	exit;
 }
 
 $email = '';
 $codigo = '';
-$sql = "SELECT email, codigo FROM usuarios WHERE id = '". addslashes($_SESSION['logado']). "'";     // ultilizando a 'SESSION' que está armazenada com o id do usuário logado para pegar o 'email'
-$sql = $pdo-> query($sql);
-if($sql-> rowCount() > 0) {
-    $info = $sql-> fetch();
-    $email = $info['email'];
-    $codigo = $info['codigo'];
+
+$sql = "SELECT email, codigo FROM usuarios WHERE id = '".addslashes($_SESSION['logado'])."'";     // ultilizando a 'SESSION' que está armazenada com o id do usuário logado para pegar o 'email'
+$sql = $pdo->query($sql);
+if($sql->rowCount() > 0) {
+	$info = $sql->fetch();
+	$email = $info['email'];
+	$codigo = $info['codigo'];
 }
 ?>
 <h1>Área interna do sistema</h1>
-<p>Usuário: <?php echo $email; ?> - <a href = "sair.php">Sair</a></p>
-<p>Link: http://localhost/projeto_registro_por_convite/cadastraa.php?codigo=<?php echo $codigo ?></p>
+<p>Usuário: <?php echo $email; ?> - <a href="sair.php">Sair</a></p>
+<p>Link: http://localhost/projeto_registroporconvite/cadastrar.php?codigo=<?php echo $codigo; ?></p>
