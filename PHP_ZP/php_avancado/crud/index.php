7c1370b496fff5a445beb3fc5faf3fcc52f5a551
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-05 01:04:11 -0300

    pegando_o_contato_salvo_no_banco_de_dados_com_base_no_seu_'email'_e_a_toda_lista_de_contatos_salvos_apresentando_os_na_tela_ultilizando_'echo'_e_'print_r'

--- PHP_ZP/php_avancado/crud/index.php
+++ PHP_ZP/php_avancado/crud/index.php
@@ -7,4 +7,10 @@ $contato = new Contato();       // intançiando a classe 'Contato' em um objeto 
 $contato-> adicionar('l.henrick@live.com', 'Luis Henrique Silva Ferreira');     // 'criando' contato
 $contato-> adicionar('l.henrick@live.co', 'LHSF');
 
+$nome = $contato-> getNome('l.henrick@live.com');       // pegando nome do contato cujo 'email' está sendo passado como parâmetro
+echo "Nome: ". $nome. "<br/>";
+
+$lista = $contato-> getAll();       // pegando a lista 'total' dos contatos
+print_r($lista);        // 'print_r' para mostrar o array inteiro
+
 ?>
