9983750ace4cbc18e3724e33a6655d94c3ca4f23
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-12 15:38:49 -0300

    'documentando_o_codigo'_criando_uma_simples_funca_de_'somar'_e_'comentarios'_explicativos_para_que_todos_consiga_entender_o_que_esta_sendo_feito_no_codigo

---
+++ PHP_ZP/php_avancado/boas_praticas.php
@@ -0,0 +1,21 @@
+<?php
+
+/*      comentário de multiplas linhas
+* Função 'somar'
+*
+* Está classe vai pega um valor 'x' e soma com o valor 'y'
+*
+* @package ControleDeEstoque
+* @author Luis Henrique Silva Ferreira <l.henrick@live.com>
+*
+* @param $x float É o primeiro valor a ser somado
+* @param $y float É o segundo valor a ser somado
+* @return float
+*
+*/
+
+function somar($x, $y) {
+    return $x + $y;
+}
+
+?>
