e0210a71f1be893707053e1f82e13bea2247e204
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-15 02:07:59 -0300

    adicionei_suporte_'multilingue'_ao_projeto_com_uma_classe_'Language'_permitindo_a_selecao_dinamica_de_idiomas_com_base_em_sessoes_e_a_traducao_de_palavras_chave

--- PHP_ZP/php_avancado/multilinguagem/Language.class.php
+++ PHP_ZP/php_avancado/multilinguagem/Language.class.php
@@ -1,15 +1,34 @@
 <?php
 
 class Language {        // classe responsável por fazer a manipulação das línguagens
-
     private $l;
+    private $ini;
 
     public function __construct() {     // classe construtora define a linguagem que está setada
-        $this-> l = 'pt-br';        // definindo português como lingagem padrão
+        $this-> l = 'pt-br';        // definindo português como linguagem padrão
 
+        if (!empty($_SESSION['lang']) && file_exists('lang/' . $_SESSION['lang'] . '.ini')) {       // verificando se 'lang' está prenchido e se existe o 'diçionario' de idioma 'lang'
+            $this-> l = $_SESSION['lang'];
+        }
+
+        $this-> ini = parse_ini_file('lang/'. $this-> l. '.ini');       // função 'parse_ini_file' lê o arquivo 'ini' e o transforma em um 'array'
     }
-    
-    public function get() {
 
+    public function getLanguage() {     // método 'getLanguage' retorna a liguagem que está definida
+        return $this-> l;
+    }
+
+    public function get($word, $return = false) {
+        $text = $word;
+    
+        if (isset($this->ini[$word])) {        // verificando se a palavra existe no array de tradução '$this->ini'
+            $text = $this->ini[$word];      // substituindo '$text' pela tradução correspondente
+        }
+    
+        if ($return) {      // Se $return for true, retorna a tradução
+            return $text;
+        } else {        // Se $return for false, imprime a tradução na saída
+            echo $text;
+        }
     }
 }
--- PHP_ZP/php_avancado/multilinguagem/index.php
+++ PHP_ZP/php_avancado/multilinguagem/index.php
@@ -1,12 +1,19 @@
 <?php
 session_start();
 
+if(!empty($_GET['lang'])) {     // verificando foi 'enviado'
+    $_SESSION['lang'] = $_GET['lang'];      // troca a linguagem
+}
+
 require 'Language.class.php';
 $lang = new Language();
 
 ?>
 <a href="index.php?lang=en">English</a>
-<a href="index.php?lang=pt">Português</a>
+<a href="index.php?lang=pt">/ Português</a>
+<a href="index.php?lang=es">/ Español</a>
+<hr/>
+Linguagem definida: <?php echo $lang-> getLanguage(); ?>
 <hr/>
 
 <button><?php echo $lang-> get('BUY'); ?></button>
