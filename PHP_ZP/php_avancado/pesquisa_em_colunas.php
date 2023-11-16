29ca532e3a7af69c377661744837e8567421247b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-16 13:41:46 -0300

    criando_comentarios_explicativos_no_codigo_psrs_facil_compreencao

--- PHP_ZP/php_avancado/pesquisa_em_colunas.php
+++ PHP_ZP/php_avancado/pesquisa_em_colunas.php
@@ -1,5 +1,5 @@
 <h1>Digite o EMAIL ou CPF do usuário</h1>
-<form method = "GET">
+<form method = "GET">       <!-- formulário de pesquisa  -->
     <input type = "text" name = "campo" />
     <input type = "submit" name = "pesquisar" />
 </form>
@@ -8,27 +8,27 @@
 
 <?php
 
-if(!empty($_GET['campo'])) {
+if(!empty($_GET['campo'])) {        // verifica se o 'campo' está ou não está vazio
     $campo = $_GET['campo'];
 
     try {
-        $pdo = new PDO("mysql:dbname=projeto_pesquisa_colunas;host=localhost", "root", "");
+        $pdo = new PDO("mysql:dbname=projeto_pesquisa_colunas;host=localhost", "root", "");     // conectando ao banco de dados 'projeto_pesquisa_colunas'
     } catch(PDOException $e) {
         echo "ERRO: ". $e-> getMessage();
         exit;
     }
 
-    $sql = "SELECT * FROM usuarios WHERE email = :email OR cpf = :cpf OR nome = :nome";
+    $sql = "SELECT * FROM usuarios WHERE email = :email OR cpf = :cpf OR nome = :nome";     // seleçionando todos os vcampos da tabela 'usuarios' onde o 'email' ou o 'cpf' for igual ao digitado no formulário
     $sql = $pdo-> prepare($sql);
     $sql-> bindValue("email", $campo);
     $sql-> bindValue("cpf", $campo);
     $sql-> bindValue("nome", $campo);
     $sql-> execute();
 
-    if($sql-> rowCount() > 0) {
-        $sql = $sql-> fetch();
+    if($sql-> rowCount() > 0) {     // verificando se a 'busca' funçionou
+        $sql = $sql-> fetch();          // obténdo o resultado da consulta
 
-        echo "Nome: ". $sql['nome'];
+        echo "Nome: ". $sql['nome'];        // exibindo na tela o nome do usuário 'pesquisado'
     }
 }
 
