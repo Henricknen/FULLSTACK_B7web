31d7da7cb01d66a75e1003d54366ecefba10f833
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-16 14:53:33 -0300

    criacao_de_projeto_que_'filtra_tabelas'_do_banco_de_dados_criando_formulario_que_exibe_os_dados_para_os_usuarios_e_exibindos_os_dados_salvos_no_banco_de_dados

---
+++ PHP_ZP/php_avancado/filtro_em_tabelas.php
@@ -0,0 +1,41 @@
+<?php
+
+try {
+    $pdo = new PDO("mysql:dbname=projeto_filtro_tabela;host=localhost", "root", "");
+} catch(PDOExecption $e) {
+    echo "ERRO: ". $e-> getMessage();    
+}
+
+?>
+<table border = "1" width = "100">
+    <tr>
+        <th>Nome</th>
+        <th>Sexo</th>
+        <th>Idade</th>
+    </tr>
+    <?php
+    $sql = "SELECT * FROM usuarios";
+    $result = $pdo->query($sql);
+
+    if ($result) {
+        $usuarios = $result->fetchAll();
+
+        if (count($usuarios) > 0) {
+            foreach ($usuarios as $usuario) {
+                ?>
+                <tr>
+                    <td><?php echo $usuario['nome']; ?></td>
+                    <td><?php echo $usuario['sexo']; ?></td>
+                    <td><?php echo $usuario['idade']; ?></td>
+                </tr>
+                <?php
+            }
+        } else {
+            echo "Nenhum usuário encontrado.";
+        }
+    } else {
+        echo "Erro na consulta SQL.";
+    }
+?>
+
+</table>
