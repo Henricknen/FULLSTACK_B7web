9331002e0261358c7b5219770d0f4bc732a41b09
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-17 16:45:27 -0300

    comentando_o_codigo_para_mais_facil_compreencao

--- PHP_ZP/php_avancado/filtro_em_tabelas.php
+++ PHP_ZP/php_avancado/filtro_em_tabelas.php
@@ -1,34 +1,37 @@
 <?php
 
 try {
-    $pdo = new PDO("mysql:dbname=projeto_filtro_tabela;host=localhost", "root", "");
-} catch(PDOExecption $e) {
-    echo "ERRO: ". $e-> getMessage();    
+    $pdo = new PDO("mysql:dbname=projeto_filtro_tabela;host=localhost", "root", "");        // conexão com 'banco de dados'
+} catch(PDOException $e) {
+    echo "ERRO: " . $e->getMessage();    
 }
 
 ?>
-<table border = "1" width = "100">
-    <tr>
+<table border="1" width="100">      <!-- tabela HTML para exibir dados do banco de dados -->
+    
+<tr>        <!-- Cabeçalhos da tabela -->
         <th>Nome</th>
         <th>Sexo</th>
         <th>Idade</th>
     </tr>
     <?php
-    $sexos = array (
+    $sexos = array(     // array associativo para mapear códigos de sexo para seus respectivos valores
         '0' => 'Masculino',
-        '1' => 'femininno'
+        '1' => 'Feminino'
     );
-    $sql = "SELECT * FROM usuarios";
+
+    $sql = "SELECT * FROM usuarios";        //  selecionando todos os registros da tabela 'usuarios'
+
     $result = $pdo->query($sql);
 
-    if ($result) {
+    if ($result) {      // verificando se a consulta foi bem-sucedida
         $usuarios = $result->fetchAll();
 
-        if (count($usuarios) > 0) {
-            foreach ($usuarios as $usuario) {
+        if (count($usuarios) > 0) {        // verificando se existem registros para exibir na tabela
+            foreach ($usuarios as $usuario) {            // 'loop' através dos registros e exibe cada um na tabela
                 ?>
                 <tr>
-                    <td><?php echo $usuario['nome']; ?></td>
+                    <td><?php echo $usuario['nome']; ?></td>          <!-- exibindo os dados do usuário em cada célula da tabela -->
                     <td><?php echo $sexos[$usuario['sexo']]; ?></td>
                     <td><?php echo $usuario['idade']; ?></td>
                 </tr>
@@ -40,6 +43,5 @@ try {
     } else {
         echo "Erro na consulta SQL.";
     }
-?>
-
+    ?>
 </table>
