3283a3dc41d4a77fffcb8df3abfd4de3ece8ddaa
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-25 22:16:20 -0300

    terminando_projeto_de_'login_unico'_onde_apenas_uma_pessoa_pode_se_logar_por_vez_no_sistema

--- PHP_ZP/php_avancado/sistema_de_login_unico/index.php
+++ PHP_ZP/php_avancado/sistema_de_login_unico/index.php
@@ -5,6 +5,20 @@ require 'config.php';
 if(empty($_SESSION['lg'])) {        // se 'lg' estiver vazio(não existir seção) será redireçionado para 'login.php'
 	header("Location: login.php");
 	exit;
-} 
+} else {
+	$id = $_SESSION['lg'];
+	$ip = $_SERVER['REMOTE_ADDR'];
+
+	$sql = "SELECT * FROM usuarios WHERE id = :id AND ip = :ip";
+	$sql = $pdo-> prepare($sql);
+	$sql-> bindValue(":id", $id);
+	$sql-> bindValue(":ip", $ip);
+	$sql-> execute();
+
+	if($sql-> rowCount() == 0) {
+		header("Location: login.php");
+		exit;
+	}
+}
 ?>
 <h1>Conteúdo Confidencial</h1>
--- PHP_ZP/php_avancado/sistema_de_login_unico/login.php
+++ PHP_ZP/php_avancado/sistema_de_login_unico/login.php
@@ -1,4 +1,38 @@
+<?php
+session_start();
+require 'config.php';
 
+$_SESSION['lg'] = '';
+
+if(isset($_POST['email']) && !empty($_POST['email'])) {		// se 'email' foi enviado e não está vazio
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+
+	$sql = "SELECT * FROM usuarios WHERE email = :email AND senha = :senha";		// verificação
+	$sql = $pdo-> prepare($sql);
+	$sql-> bindValue(":email", $email);
+	$sql-> bindValue(":senha", $senha);
+	$sql-> execute();
+
+	if($sql-> rowCount() > 0) {		// verificando se houve 'resultado' da consulta
+		$sql = $sql-> fetch();
+		$id = $sql['id'];		// pegando o 'id' do usuário
+		$ip = $_SERVER['REMOTE_ADDR'];		// 'REMOTE_ADDR' variável que pega o 'ip' do cliente que está acesaando o servidor web
+
+		$_SESSION['lg'] = $id;
+
+		$sql = "UPDATE usuarios SET ip = :ip WHERE id = :id";		// Atualiza a tabela 'usuarios' definindo o campo 'ip' para um valor específico, com base no 'id'
+		$sql = $pdo-> prepare($sql);
+		$sql-> bindValue(":ip", $ip);
+		$sql-> bindValue(":id", $id);
+		$sql-> execute();
+
+		header("Location: index.php");
+		exit;
+	}
+}
+
+?>
 <h1>Login</h1>		<!-- formulário de 'login' -->
 <form method = "POST">
 	E-mail:<br/>
