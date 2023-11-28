d7eaf6f9f6fdaf3d0dad1365c1a537479fd1d371
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-28 16:40:45 -0300

    terminando_projeto_criando_logica_para_contar_'acessos'_feitos_no_sistema_e_gravando_no_'bd'

--- PHP_ZP/php_avancado/usuarios_online/index.php
+++ PHP_ZP/php_avancado/usuarios_online/index.php
@@ -8,3 +8,15 @@ $sql = $pdo-> prepare("INSERT INTO acessos (ip, hora) VALUES (:ip, :hora)");    
 $sql-> bindValue(":ip", $ip);
 $sql-> bindValue(":hora", $hora);
 $sql-> execute();
+
+$sql = $pdo-> prepare("DELETE FROM acessos WHERE hora < :hora");     // deletando acessos onde a gora for menor que 2 minutos atrás
+$sql-> bindValue(":hora", date('H:i:s', strtotime("-2 minutes")));
+$sql-> execute();
+
+$sql = "SELECT * FROM acessos WHERE hora > :hora GROUP BY ip";      // pegando todos 'acessos' válidos e 'GROUP BY' agrupa pelo 'ip'
+$sql = $pdo-> prepare($sql);
+$sql-> bindValue(":hora", date('H:i:s', strtotime("-2 minutes")));      // 'strtotime' transforma uma 'string' texo em um 'int' tempo real
+$sql-> execute();
+$contagem = $sql-> rowCount();
+
+echo "OnLine: ". $contagem;
