2d2deccd55ee0f7825962832db4b9997f2070d8f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-29 12:14:57 -0300

    criando_verificacao_para_garantir_que_os_campos_do_formulário_de_login_foram_preenchidos_alem_de_realizar_a_correspondencia_segura_de_email_e_senha_no_banco_de_dados_com_protecao_contra_injecao_de_SQL_e_preparacao_segura_de_consultas

--- PHP_ZP/php_avancado/marketing_multinivel/login.php
+++ PHP_ZP/php_avancado/marketing_multinivel/login.php
@@ -1,6 +1,22 @@
 <?php
 session_start();
 require 'config.php';
+
+if(!empty($_POST['email'])) {       // se campo de 'email' não estiver vazio
+    $email = addslashes($_POST['email']);
+    $senha = addslashes($_POST['senha']);
+
+    $sql = $pdo-> prepare("SELECT * FROM usuarios WHERE email = :email AND senha = :senha");
+    $sql-> bindValue(":email", $email);
+    $sql-> bindValue(":senha", $senha);
+    $sql-> execute();
+
+    if($sql-> rowCount() > 0) {
+
+    } else {
+        echo "Usuário e/ou Senha errados...";
+    }
+}
 ?>
 
 <form method="POST">
