cb5a2f8c23cd9d244ac50b09816604fec2c6bde8
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-11-30 13:31:44 -0300

    mostrando_nome_do_usuario_logado_atraves_do_'id'

--- PHP_ZP/php_avancado/marketing_multinivel/index.php
+++ PHP_ZP/php_avancado/marketing_multinivel/index.php
@@ -9,9 +9,20 @@ if(empty($_SESSION['mmnlogin'])) {          // verificando se a seção está va
 
 $id = $_SESSION['mmnlogin'];
 
-$sql = $pdo-> prepare("SELECT nome FROM usuarios WHERE id = :id");      // puchando nome do usuário logado
+$sql = $pdo-> prepare("SELECT nome FROM usuarios WHERE id = :id");      // puchando nome do usuário logado através do 'id'
 $sql-> bindValue(":id", $id);
 $sql-> execute();
 
+if($sql-> rowCount() > 0) {     // verificando se encontrou algum resultado
+    $sql = $sql-> fetch();
+    $nome = $sql['nome'];
+} else {
+    header("Location: login.php");
+    exit;
+}
+?>
 
-// - 16:27
+<h1>Sistema de Marketing Multinível</h1>
+<h2>Usuário logado: <?php echo $nome; ?></h2>
+
+<a href=  "cadastro.php">Cadastrar novo usuário</a>
