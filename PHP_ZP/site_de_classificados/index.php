a165c72eef275873ab4171be7814ad5d5a6c7be1
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-12-04 23:30:14 -0300

    criando_um_'site_de_classificados'_em_'php'_ultilizando_a_biblioteca_'bootstrap'

---
+++ PHP_ZP/site_de_classificados/index.php
@@ -0,0 +1,19 @@
+<?php require 'pages/header.php'; ?>
+
+    <div class = "container-fluid">
+        <div class = "jumbotron">
+            <h2>Nós temos hoje 999 anúncios.</h2>
+            <p>E mais de 999 usuários cadastrados.</p>
+        </div>
+
+        <div class = "row">
+            <div class = "col-sm-3">
+                <h4>Pesquisa avançada</h4>
+            </div>
+            <div class = "col-sm-9">
+                <h4>Últimos anúncios</h4>
+            </div>
+        </div>
+    </div>
+
+<?php require 'pages/footer.php'; ?>
---
+++ PHP_ZP/site_de_classificados/pages/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang = "pt-br">
+<head>
+    <meta charset = "UTF-8">
+    <meta http-equiv = "X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classificados</title>
+    <link rel = "stylesheet" href = "assets/css/bootstrap.min.css" />       <!-- puchando bootstrap de 'css' -->
+    <link rel = "stylesheet" href = "assets/css/style.css" />
+    <script type = "text/javascript" src = "assets/js/jquery.min.js"></script>
+    <script type = "text/javascript" src = "assets/js/bootstrap.min.js"></script>     <!-- puchando bootstrap de 'js' -->
+    <script type = "text/javascript" src = "assets/js/script.js"></script>
+</head>
+<body>
+    <nav class = "navbar navbar-inverse">
+        <div class = "container-fluid">
+            <div class = "navbar-header">
+                <a href = "./" class = "navbar-brand">Classificados</a>
+            </div>
+            <ul class = "nav navbar-nav navbar-right">     <!-- colocando botões do lado direito da tela -->
+                <li><a href = "">Cadastre-se</a></li>
+                <li><a href = "">Login</a></li>
+            </ul>
+        </div>
+
+    </nav>
