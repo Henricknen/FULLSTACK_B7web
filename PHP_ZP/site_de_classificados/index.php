ae435994c302890b20c05c236d572f920c2382ee
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-12-06 00:00:03 -0300

    comentando

--- PHP_ZP/site_de_classificados/index.php
+++ PHP_ZP/site_de_classificados/index.php
@@ -1,17 +1,19 @@
 <?php require 'pages/header.php'; ?>
 
-    <div class = "container-fluid">
-        <div class = "jumbotron">
+    <div class="container-fluid">
+        
+        <div class="jumbotron">     <!-- 'jumbotron' exibindo informações sobre anúncios e usuários cadastrados -->
             <h2>Nós temos hoje 999 anúncios.</h2>
             <p>E mais de 999 usuários cadastrados.</p>
         </div>
 
-        <div class = "row">
-            <div class = "col-sm-3">
+        <div class="row">       <!-- divisão em duas colunas: pesquisa avançada e últimos anúncios -->
+            <div class="col-sm-3">      <!-- coluna para pesquisa avançada -->
                 <h4>Pesquisa avançada</h4>
-            </div>
-            <div class = "col-sm-9">
-                <h4>Últimos anúncios</h4>
+            </div>      <!-- aqui você pode adicionar elementos relacionados à pesquisa avançada -->
+
+            <div class="col-sm-9">      <!-- coluna para os últimos anúncios -->
+                <h4>Últimos anúncios</h4>       <!-- aqui você pode adicionar elementos relacionados aos últimos anúncios -->
             </div>
         </div>
     </div>
