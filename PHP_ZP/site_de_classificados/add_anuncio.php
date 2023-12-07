2dcde95d7b02901dcd3a302ecce7ee0c78341a6c
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-12-07 19:16:39 -0300

    criando_formulario_para_'adicionar'_anuncios'

--- PHP_ZP/site_de_classificados/add_anuncio.php
+++ PHP_ZP/site_de_classificados/add_anuncio.php
@@ -11,6 +11,40 @@ if(empty($_SESSION['cLogin'])) {
 
 <div class="container">
     <h1>Meus anuçios - Adicionar anuncio</h1>
+
+    <form method="POST" enctype = "multi/form-data">        <!-- 'multi/form-data' permite inserir imagens -->
+
+    <div class="form-group">
+        <label for = "categoria">Categoria:</label>
+        <selct name = "categoria" id = "categoria" class="form-control" />
+            <option></option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for = "titulo">Título:</label>
+        <input type="text" name = "titulo" id = "titulo" class="form-control" />
+        </select>
+    </div>
+    <div class="form-group">
+        <label for = "valor">Valor:</label>
+        <input type="text" name = "valor" id = "valor" class="form-control" />
+        </select>
+    </div>
+    <div class="form-group">
+        <label for = "descricao">Descrição:</label>
+        <input type="text" name = "descricao" id = "descricao" class="form-control" />
+        </select>
+    </div>
+    <div class="form-group">
+        <label for = "estado">Estado de conservação:</label>
+        <selct name = "estado" id = "estado" class="form-control" />
+            <option value = "0">Ruim</option>
+            <option value = "1">Bom</option>
+            <option value = "2">Ótimo</option>
+        </select>
+    </div>
+    <input type="submit" value = "Adicionar" class="btn btn-default" />
+    </form>
 </div
 
 <?php require 'pages/footer.php'; ?>
