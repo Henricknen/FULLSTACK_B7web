37686419153d5fd4dac4c38e00b59f784dda2e4a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-12-31 14:05:19 -0300

    tag_'iframe'_se_transforma_em_um_mini_navegador_que_pode_ser_inserido_dentro_do_site

---
+++ FRONTEND_ZP/html_e_css/iframe/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iframe</title>
+</head>
+<body>
+    <!-- <iframe src="https://www.github.com/henricknen" width = "500" height = "400"></iframe> -->  <!-- 'src' espeçficando uma página externa 'width' a largura e 'height' a altura -->
+    <iframe frameborder = "0" src = "teste.html" width = "500" height = "400"></iframe>       <!-- parâmetro 'frameborder' altera a borda -->
+</body>
+</html>
