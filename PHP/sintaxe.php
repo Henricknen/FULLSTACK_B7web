c5fb212e735e40a92b1447e8f9f0823ecbf5351e
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-01-02 19:43:37 -0300

    criando_sintaxe_basica_para_um_perfeito_entendimento_de_codigos_php

---
+++ PHP/sintaxe.php
@@ -0,0 +1,16 @@
+<?php       // tudo o que estiver dentro da tag '<?php' é codigo php que é interpretado dentro do servidor
+
+$nome = "Luis Henrique Silva Ferreira";     // '$' define uma variável
+$formacao = "Análise e Desenvolvimento de Sistemas";
+$cidade = 'Martinópolis-SP';
+
+echo "Me chamo ". $nome. " sou formado em ". $formacao. " moro na cidade de ". $cidade;        // comando 'echo' mostra as informações na tela '.' é usado para concatenar
+
+// comentário de uma linha
+
+/*
+comentário
+de várias linhas
+*/
+
+?>
