54e56e77ad281b48da63e954d571f897b3f901f3
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-01-04 20:54:15 -0300

    'variaveis_globais'_sao_arrays_que_ja_vem_incorporado_no_proprio_sistema_php

---
+++ PHP/variaveis_e_arrays/variaveis_globais.php
@@ -0,0 +1,4 @@
+<?php
+$nome = $_GET["nome"];      // ultilizabdo variável global '$_GET' e transferindo para variável '$nome' toda variável global é precedida pelo símbolo '$'
+
+echo "Seu nome é ". $nome;
