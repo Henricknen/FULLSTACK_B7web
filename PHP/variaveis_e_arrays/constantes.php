778e5e0f62e75f4efc0b414a420f66800c59dc28
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-01-05 23:10:10 -0300

    ultilizando_'define'_para_criar_constantes_que_sao_inalteraveis__e_que_podem_ser_usadas_posteriormente_sem_a_necessidade_de_redefini_las

---
+++ PHP/variaveis_e_arrays/constantes.php
@@ -0,0 +1,6 @@
+<?php
+define("URL", "https://github.com/Henricknen/FULLSTACK_B7web/tree/main/PHP");       // função 'define' define uma variável constante
+define("VERSION", "1.0");       // também é ultilizado para definir a 'versão' do sistema
+
+echo "Meu repositório de php eh: ". URL;        // costante não tem o '$'
+echo "Versão: ". VERSION;
