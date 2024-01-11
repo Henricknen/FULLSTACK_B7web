53a6068686ccfd541c8dec0239c9a5e9fcde8e4f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-01-11 01:04:03 -0300

    formatando_comentario_e_criando_o_formulario_para_adicionar_os_comentarios

--- WORDPRESS/wp-content/themes/primeiro_tema/comments.php
+++ WORDPRESS/wp-content/themes/primeiro_tema/comments.php
@@ -7,17 +7,30 @@ if(have_comments()) {       // verificando se existe comentários
     foreach($comments as $comment) {
         ?>
 
-        <div class="comentario">        <!-- exibindo os comentários -->
-            <div class="comentario_foto">
+        <div class = "comentario">        <!-- exibindo os comentários -->
+            <div class = "comentario_foto">
                 <?php echo get_avatar($comment, 60); ?>
             </div>
-            <div class="comentario_area">
-                <strong><?php comment_author(); ?></strong> - <?php comment_date(); ?><br><br>
+            <div class=  "comentario_area">
+                <strong><?php comment_author(); ?></strong> - <?php comment_date(); ?><br>      <!-- nome do autor e data do comentario -->
                 <?php comment_text(); ?>        <!-- exibe o comentario do autor -->
             </div>
         </div>
 
         <?php
     }
+
+    the_comments_pagination();      // páginação[1, 2, 3, 4, ....]
 }
-?>
+
+comment_form(array(     // função cria um formulário
+    'comment_field'=> 'Comentario:<br/><textarea name = "comment"></textarea><br/>',       // inserindo um textarea
+    'filds'=> array(        // 'filds' outros campos
+        'author'=> 'Nome: <input type = "text" name = "author" placeholder = "Digite seu nume" /><br/>',
+        'email'=> 'Email: <input type = "email" name = "email" placeholder = "Digite seu Email" /><br/>',
+        'url'=> 'Url: <input type = "url" name = "url" placeholder = "Digite seu  Site" /><br/>'
+    ),
+    'class_submit'=> 'botao',     // classe do botão de submit
+    'label_submit'=> 'Envie seu comentário',        // alterando o texto do botão
+    'title_reply'=> 'Deixe um comentário'
+)); 
