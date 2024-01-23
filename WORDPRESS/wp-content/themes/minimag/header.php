b046ba61a240df4469f3205f1815e099f5c3c581
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-01-22 21:10:28 -0300

    criando_o cabecalho_do_site_criando_o_menu_do_'header'

--- WORDPRESS/wp-content/themes/minimag/header.php
+++ WORDPRESS/wp-content/themes/minimag/header.php
@@ -2,4 +2,26 @@
     <head>
         <?php wp_head(); ?>
     </head>
+    <body <?php body_class(); ?>>
+        <header>
+
+            <div class="top_header">
+                <nav class = "navbar navbar-default">
+                    <div class = "container-fluid">       <!-- classe deixa o conteúdo fluído ficar responsivo -->
+                        <div class = "collapse navbar-collapse">
+                            <?php
+                                if (has_nav_menu('top')) {        // se existir menu top
+                                    wp_nav_menu(array(
+                                        'theme_location' => 'top',
+                                        'container'=> false,
+                                        'fallback_cb'=> false 
+                                    ));
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            
+        </header>
 </html>
--- WORDPRESS/wp-content/themes/minimag/index.php
+++ WORDPRESS/wp-content/themes/minimag/index.php
@@ -1,1 +1,1 @@
-<?php get_header(); ?>
+<?php get_header();
