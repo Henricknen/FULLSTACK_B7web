2896069d1b3738ac9c333cab6d01879730d86566
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-02-08 14:38:46 -0300

    adicionado_testes_utilizando_'@dataProvider'_para_validar_diferentes_cenarios_de_entrada

--- PHP_ZP/php_orientado_a_testes/tests/CalculadoraTest.php
+++ PHP_ZP/php_orientado_a_testes/tests/CalculadoraTest.php
@@ -3,31 +3,25 @@ use PHPUnit\Framework\TestCase;
 
 class CalculadoraTest extends TestCase {    // classe de teste extendendo funçionalidade do 'php unit'
 
-    public function testSoma() {        // método de teste 'testSoma'
+    /**
+    * @dataProvider somaDataProvider
+    */
+
+    public function testSoma($n1, $n2, $esperado) {        // método de teste 'testSoma'
         $calc = new Calculadora();
 
         $this-> assertEquals(     // método 'assertEquals' realiza o teste
-            2,      // este parâmetro é o que se espera como resultado
-            $calc-> soma(1, 1)        //  '$calc-> soma(1, 1)' é a execução da calculadora  
+            $esperado,      // este parâmetro é o que se espera como resultado
+            $calc->soma($n1, $n2)        //  '$calc-> soma(1, 1)' é a execução da calculadora  
         );
     }
     
-    public function testSoma2() {       // segundo teste
-        $calc = new Calculadora();
-
-        $this-> assertEquals(
-            -5,
-            $calc-> soma(-10, 5)
+    public static function somaDataProvider(): array {        // função adiçional 'somaDataProvider' retorna um array de testes
+        return array(
+            array(1, 2, 3),     // o 'primeiro' e o 'segundo' parâmetros são os dois números que serão somados e o 'terçeiro' é o resultado que se espera da soma
+            array(50, 53, 103),
+            array(87, 23, 110),
+            array(35, 25, 75)       // array dará erro pois os valores passados não batem com o resultado esperado
         );
     }
-    
-    public function testSoma3() {       // simulando 'erro'
-        $calc = new Calculadora();
-
-        $this-> assertEquals(
-            35,     // resultado diferente do esperado
-            $calc-> soma(10, 15)
-        );
-    }
-
 }
