3f53fc232233f0085eb31222a91533830ada427f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-02-23 10:56:29 -0300

    criando_um_site_em_'php'_ultilizando_linguagem_de_marcacao_'html'_para_definir_sua_estrutura

---
+++ PHP/site/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang = "pt-br">
+<head>      <!-- seção de cabeçalho -->
+    <meta charset = "UTF-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <title>Site em PHP</title>
+    <link rel = "stylesheet" type =  "text/css" href = "css/style.css">     <!-- folha de estilo externa -->
+</head>
+<body>      <!-- corpo -->
+    <div id = "topo">
+        topo
+    </div>
+
+    <div id = "menu">
+        menu
+    </div>
+
+    <div id = "conteudo">
+        conteudo
+    </div>
+
+    <div id = "rodape">
+        rodape
+    </div>
+</body>
+</html>
