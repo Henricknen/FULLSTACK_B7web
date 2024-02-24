a9a7cb8f633ee2d36eac6c885868491c33594642
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-02-24 12:08:20 -0300

    criando_o_topo_do_ site_e_inserindo_'logo'_e_'links'_de_se_cadastrar_e_logar

--- PHP/site/index.php
+++ PHP/site/index.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
-<html lang = "pt-br">
-<head>      <!-- seção de cabeçalho -->
-    <meta charset = "UTF-8">
-    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
-    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-    <title>Site em PHP</title>
-    <link rel = "stylesheet" type =  "text/css" href = "css/style.css">     <!-- folha de estilo externa -->
-</head>
-<body>      <!-- corpo -->
-    <div id = "topo">
-        topo
-    </div>
+    <html lang = "pt-br">
+    <head>      <!-- seção de cabeçalho -->
+        <meta charset = "UTF-8">
+        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+        <title>Site em PHP</title>
+        <link rel = "stylesheet" type =  "text/css" href = "css/style.css">     <!-- folha de estilo externa -->
+    </head>
+    <body>      <!-- corpo -->
+    <div id = "geral">
+        <div id = "topo">
+            <?php include "topo.php";?>     <!-- 'incluindo' o arquivo topo.php -->
+        </div>
 
-    <div id = "menu">
-        menu
-    </div>
+        <div id = "menu">
+            menu
+        </div>
 
-    <div id = "conteudo">
-        conteudo
-    </div>
+        <div id = "conteudo">
+            conteudo
+        </div>
 
-    <div id = "rodape">
-        rodape
+        <div id = "rodape">
+            rodape
+        </div>
     </div>
-</body>
+    </body>
 </html>
