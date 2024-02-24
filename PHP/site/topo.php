1aae672bc8a38d3553226b6ed149e8dc9e164882
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-02-24 15:22:16 -0300

    'estilizando'_os_links_do_topo

--- PHP/site/topo.php
+++ PHP/site/topo.php
@@ -1,3 +1,3 @@
 <img src = "img/logo.png" class = "logo">
-<a href = "cadastro.php" class = "links_top">Cadatra-se</a>
 <a href = "form_login" class = "links_top">Logar</a>
+<a href = "cadastro.php" class = "links_top">Cadatra-se</a>
