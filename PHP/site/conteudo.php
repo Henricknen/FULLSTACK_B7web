93d5903226733cca2e43b409207a5863174b6717
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-02-26 11:14:59 -0300

    inserindo_'titulos'_'imagens'_no_conteudo_do_site_e_fazendo_a_estilazacao_com_'css'

--- PHP/site/conteudo.php
+++ PHP/site/conteudo.php
@@ -1,5 +1,17 @@
 <div id = "conteudo_pricipal">
-
+    <div class = "postagens">
+        <h1 class = "titulos">Titulo</h1>
+        <img src = "img/php.jpg" class = "imagem">
+        <p class = "paragrafo">kadfkldaçdkjdçdj</p>
+        <span class = "data">26/02/2024</span>
+    </div>
+    
+    <div class = "postagens">
+        <h1 class = "titulos">Titulo</h1>
+        <img src = "img/laravel.jpg" class = "imagem">
+        <p class = "paragrafo">kadfkldaçdkjdçdj</p>
+        <span class = "data">26/02/2024</span>
+    </div>
 </div>
 
 <div id = "recentes">
