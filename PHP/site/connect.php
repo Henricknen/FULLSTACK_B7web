ec4e77be31998d8cf2075f7a8e6342fc51ae30b5
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-02-29 17:42:35 -0300

    criando_arquivo_'connect.php'_que_faz_a_conexao_com_o_banco_de_dados

---
+++ PHP/site/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";        // endereço do servidor MySQL
+$user = "root";     // nome de usuário do MySQL
+$pass = "";     // senha do MySQL vazia
+$db = "bd_site_php";        // nome do banco de dados
+
+$link = mysqli_connect($host, $user, $pass, $db);       // estabelecimento da conexão com o banco de dados
+
+?>
---
+++ PHP/site/consulta.sql
@@ -0,0 +1,1 @@
+select nome, email from tb_user order by id_user asc;		-- 'select' seleçiona o campo nome e email 'from' da tbela tb_user
