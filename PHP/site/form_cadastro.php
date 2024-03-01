60aaec5ae449f5f51cd50ad8cadf2b04d9ddc0a5
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-01 15:46:17 -0300

    'formatando'_o_formulario_de_cadastro

--- PHP/site/form_cadastro.php
+++ PHP/site/form_cadastro.php
@@ -8,14 +8,19 @@
         <link rel = "stylesheet" type =  "text/css" href = "css/style.css">
     </head>
     <body>
-        <form action = "cadastrar.php" method = "POST" enctype = "multipart/form-data">     <!-- formulário para usuário prencher para cadastrar-se -->
-        <input type = "text" name = "nome" class = "campos_cad" placeholder = "Nome">
-        <input type = "email" name = "email" class = "campos_cad" placeholder = "Email">
-        <input type = "password" name = "senha" class = "campos_cad" placeholder = "Senha">
-        <input type = "password" name = "reptsenha" class = "campos_cad" placeholder = "Confirmar senha">
-        <input type = "text" name = "lembrete" class = "campos_cad" placeholder = "Lembrete">
-        <input type = "file" name = "foto" class = "campos_cad">    
-        <input type = "submit" value = "Cadastrar" class = "bt_cad">
-        <input type = "reset" value = "Limpar" class = "bt_cad">
-    </form>
+        <div id="box_form">
+            <h1 class = "titulos" style = "margin-left:10%">Cadastra-se</h1>       <!-- ultilizando 'css inline' para inserir uma margem de 10% no título -->
+            <form action = "cadastrar.php" method = "POST" enctype = "multipart/form-data">     <!-- formulário para usuário prencher para cadastrar-se -->
+                <input type = "text" name = "nome" class = "campos_cad" placeholder = "Nome">
+                <input type = "email" name = "email" class = "campos_cad" placeholder = "Email">
+                <input type = "password" name = "senha" class = "campos_cad" placeholder = "Senha">
+                <input type = "password" name = "reptsenha" class = "campos_cad" placeholder = "Confirmar senha">
+                <input type = "text" name = "lembrete" class = "campos_cad" placeholder = "Lembrete">
+                <input type = "file" name = "foto" class = "campos_cad">
+                <div id = "botoes">
+                    <input type = "submit" value = "Cadastrar" class = "bt_cad">
+                    <input type = "reset" value = "Limpar" class = "bt_cad">
+                </div>
+            </form>
+        </div>
     </body>
