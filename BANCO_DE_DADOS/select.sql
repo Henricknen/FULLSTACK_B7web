0f496cdfdf0ca43a73fa8a9fe56d2f173ae47d60
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-04 09:36:09 -0300

    comando_'select'_e_ultilizado_para_obter_informacoes_do_banco_de_dados

---
+++ BANCO_DE_DADOS/select.sql
@@ -0,0 +1,4 @@
+USE crm;
+
+SELECT * FROM produtos;		-- 'SELCT' é o comando para recuperar dados do banco de dados 'FROM' espeçifica de qual tabela os dados serão recuperados
+-- '*' é um caractere curiga que signifia e pega todas a colunas da tabela espeçificada
