0100b9b5167d9a5135047b85aa6d739cde1a014e
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-03 22:39:32 -0300

    mostrando_janela_de_'javascript'_caso_senhas_sejam_diferentes_ou_caso_haja_preencimento_do_formulario_estiver_incompleto

--- PHP/site/cadastrar.php
+++ PHP/site/cadastrar.php
@@ -9,10 +9,20 @@ $tipo = $_FILES['foto']['type'];            // pegando o tipo 'type' do campo 'f
 
 // verificando se os 'valores prenchidos' no formulário estão 'chegando' neste arquivo casdastrar.php
 
-echo "Nome: ". $nome. "</br>";
-echo "Email: ". $email. "</br>";
-echo "Senha: ". $senha. "</br>";
-echo "Repetição da denha: ". $reptsenha. "</br>";
-echo "Lembrete: ". $lembrete. "</br>";
-echo "Foto: ". $foto. "</br>";
-echo "Tipo: ". $tipo. "</br>";
+// echo "Nome: ". $nome. "</br>";
+// echo "Email: ". $email. "</br>";
+// echo "Senha: ". $senha. "</br>";
+// echo "Repetição da denha: ". $reptsenha. "</br>";
+// echo "Lembrete: ". $lembrete. "</br>";
+// echo "Foto: ". $foto. "</br>";
+// echo "Tipo: ". $tipo. "</br>";
+
+if($nome != "" && $email != "" && $senha != "" && $lembrete != "") {      // verificando se as variáveis não estão vazias
+    if($senha != $reptsenha) {      // teste verifica se senhas são iguais
+        echo "<script>alert('Senhas diferentes');window.history.go(-1)</script>";     // inserindo um 'javascript' no 'php' 'window' da acesso ao objeto janela 'history' acessa o historico e go(-1) indica que o navegador deve retroceder uma página
+    } else {
+        echo "Ok";
+    }
+} else {
+    echo "<script>alert('É necessario preencher todos os campos');window.history.go(-1)</script>";
+}
