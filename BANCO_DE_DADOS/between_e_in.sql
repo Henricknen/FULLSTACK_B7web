9daabfc77ead9ab3b447806a807e413ea628c946
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-07 00:44:53 -0300

    clausula_'BETWEEN'_e_ultilizada_para_selecionar_valores_dentro_de_um_intervalo_especifico_e_a_'IN'_para_especificar_multiplos_valores_em_uma_codicao_de_pesquisa

---
+++ BANCO_DE_DADOS/between_e_in.sql
@@ -0,0 +1,3 @@
+SELECT * FROM produtos WHERE estoque BETWEEN 150 AND 250;		-- 'BETWEEN' significa 'ENTRE'
+
+SELECT * FROM produtos WHERE preco IN (400, 600, 2500);		-- 'IN' pega os valores especificos '400, 600 e 2500'
