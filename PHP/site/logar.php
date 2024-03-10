c3da0a7db6a9ec044f0efab704838f7875b3271c
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-09 22:37:59 -0300

    verificando_se_email_existe_na_'tabela'_do_banco_de_dados_e_ultilizando_funcao_'mysqli_num_rows'_para_localizar_o_numero_de_linhas_localizados_na_consulta

--- PHP/site/logar.php
+++ PHP/site/logar.php
@@ -4,7 +4,10 @@ $email = $_POST['email'];       // variável '$email' reçebe o que vem do campo
 $senha = $_POST['senha'];
 
 if($email != ""  && $senha != "") {     // verificando se variáveis estão preenchidas
-    echo "Usuario logado";
+    // echo "Usuario logado";
+    $sql = mysqli_query($link, "SELECT * FROM tb_user WHERE email = '$email'");     // trazendo todos os campos da tabela 'tb_user' onde o campo 'email' é igual ao '$email' forneçido pelo usúario
+    $registro = mysqli_num_rows($sql);      // função 'mysqli_num_rows' verifica quantas linhas foi encontrada
+    echo $registro; 
 } else {
     echo "É necessario preencher todos os campos e possuir um cadastro";
 }
