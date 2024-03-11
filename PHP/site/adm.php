81b5a3c1f6f073de220b5784545cbcd13507b625
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-11 14:01:11 -0300

    criacao_da_tela_de_'administrador'

---
+++ PHP/site/adm.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang = "pt-br">
+    <head>
+        <meta charset = "UTF-8">
+        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+        <title>Site em PHP</title>
+        <link rel = "stylesheet" type =  "text/css" href = "css/style.css">
+    </head>
+    <body>
+        <div id="box_log">
+            <h1 class = "titulos" style = "margin-left:2%">Bem vindo a tela de ADM</h1>
+           
+        </div>
+    </body>
--- PHP/site/logar.php
+++ PHP/site/logar.php
@@ -9,12 +9,18 @@ if($email != ""  && $senha != "") {     // verificando se variáveis estão pree
     $registro = mysqli_num_rows($sql);      // função 'mysqli_num_rows' verifica quantas linhas foi encontrada
     while($line = mysqli_fetch_array($sql)) {        // variável '$line' armazena registros que a função 'mysqli_fetch_array' localizar dentro da tabela 'tb_user'
         $senha_user = $line['senha'];
+        $nivel = $line['nivel'];
     }
     if($registro) {     // o 'if' já verifica se a condição já é 'true'
         if($senha_user == $senha) {
             SESSION_START();        // iniçiando uma sessao
             $_SESSION['login'] = $email;      // guardando o email da pessoa na sessão 'login' da variável de ambiente '$_SESSION' 
             $_SESSION['password'] = $senha;
+            if($nivel == 1) {
+                header('location:adm.php');
+            } else {
+                header('location:cliente.php');     // se o '$nivel' for diferente de 1 será apenas um cliente
+            }
         } else {
             echo "Senha não confere com a cadastra...";
             echo "<a href = 'login.php'>[Voltar]</a>";
