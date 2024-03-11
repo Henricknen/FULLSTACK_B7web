c32ac506c7339215edbe4da72fd9939f2e2cbbdc
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-10 21:01:58 -0300

    'group by'_e_ultilizado_para_obter_'estatisticas'_'somas'_'numeros'_ou_'processamentos_de_dados'_serve_para_agrupar_a_query_que_sera_executada_anteriormente_e_sempre_e_sera_ultilizado_com_alguma_contagem_media_soma

--- BANCO_DE_DADOS/group_by.sql
+++ BANCO_DE_DADOS/group_by.sql
@@ -1,2 +1,2 @@
    -- soma do estoque     			nome das colunas			agrupando pelo id_fornecedores
-SELECT SUM(estoque) AS estoqueTotal, id_fornecedor FROM produtos GROUP BY id_fornecedor;		-- 'group by' agrupa o estoqueTotal pelo 'id_fornecedor'
+SELECT SUM(estoque) AS estoqueTotal, id_fornecedor FROM produtos GROUP BY id_fornecedor;		-- 'group by' agrupa o estoqueTotal pelo 'id_fornecedor' 
