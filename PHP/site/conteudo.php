43ec78fe271e77e6e4b2f8022dc9cb1ee45a86c9
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-24 15:15:44 -0300

    iniciando_a_criacao_da_'paginacao'_na_pagina_home

--- PHP/site/conteudo.php
+++ PHP/site/conteudo.php
@@ -1,5 +1,5 @@
 <div id = "conteudo_pricipal">
-    <div class = "postagens">
+    <!-- <div class = "postagens">
         <h1 class = "titulos">Titulo</h1>
         <img src = "img/php.jpg" class = "imagem">
         <p class = "paragrafo">kadfkldaçdkjdçdj</p>
@@ -11,7 +11,27 @@
         <img src = "img/laravel.jpg" class = "imagem">
         <p class = "paragrafo">kadfkldaçdkjdçdj</p>
         <span class = "data">26/02/2024</span>
-    </div>
+    </div> -->
+
+    <?php
+        include "connect.php";
+        $qtde_registros = 1;        // variável indica a quantidade de registros que será exibidos da tabela do banco de dados
+        @$page = $_GET['pag'];
+        if(!$page) {
+            $pagina = 1;        // se variável '$page' estiver vazia é criada uma variável chamada '$pagina' contendo o valor 1
+        } else {
+            $pagina = $page;
+        }
+
+        $inicio = $pagina - 1;
+        $inicio = $inicio + $qtde_registros;
+        $sel_parcial = mysqli_query($link, "select * from tb_postagens limit $inicio, $qtde_registros");        // fazendo uma busca dentro da tabela 'tb_postagens' limitando a busca ultilizanso 'limit'
+        $sel_total = mysqli_query($link, "select * from tb_postagens");     // fazendo uma busca de todos os registros de dentro da tabela
+
+        $contar = mysqli_num_rows($sel_total);      // conta a quantidade de registro da tabela
+        $contar_pages = $contar / $qtde_registros;
+        echo $contar_pages;
+    ?>
 </div>
 
 <div id = "recentes">
