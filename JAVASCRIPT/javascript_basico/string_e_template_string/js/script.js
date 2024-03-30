dcde26f4cae8bbda5ea462818243af564fe16225
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-30 16:50:41 -0300

    criando_'string'_e_'template_string'_que_e_uma_string_onde_e_possivel_fazer_algumas_alteracoes_dentro_dela

---
+++ JAVASCRIPT/javascript_basico/string_e_template_string/js/script.js
@@ -0,0 +1,13 @@
+let pos_graduacao = "Desenvolvimento web";
+let graduacao = "Análise e desenvolvimento de sistemas";
+
+let formacao = 'pos: ' + pos_graduacao + ' e graduação: ' + graduacao;
+
+console.log(formacao);
+
+let tecnicoo = "Eletroeletronica";
+let tecnico = "Informatica para internet";
+
+let formacao_tecnica = `${tecnico} e ${tecnicoo}`;      // em template string se ultiliza ``
+
+console.log('formação tecnica: ' + formacao_tecnica);
