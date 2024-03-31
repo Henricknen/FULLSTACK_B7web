d37fc88bd2d4bd2c3581020796c3d1d0d36b6d3a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-31 18:49:58 -0300

    criando_condicionais_ultilizando_&&_e_||

---
+++ JAVASCRIPT/javascript_basico/condicional_&&/js/script.js
@@ -0,0 +1,9 @@
+let idade = 90;
+
+if(idade >= 18 && idade < 60) {      // '&&' que significa 'e' permite ter multiplas condições em apenas um só if
+    console.log("Voçe é um adulto...");
+}
+
+if(idade >= 18 || idade < 60) {      // '||' que significa 'ou'
+    console.log("Voçe é um adulto...");
+}
