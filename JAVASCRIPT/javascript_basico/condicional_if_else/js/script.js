04033d941b89e7f6426df4d8e1334c3a590d9c8f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-30 22:47:39 -0300

    criando_condicional_'if'_'else'_em_javascript

---
+++ JAVASCRIPT/javascript_basico/condicional_if_else/js/script.js
@@ -0,0 +1,13 @@
+let idade = 90;
+
+if(idade > 17) {        // codiçional 'if/else'
+    console.log("É maior de idade!");
+} else {
+    console.log("É menor de idade!");
+}
+
+if(idade >= 18) {
+    console.log("É maior de idade!");
+} else {
+    console.log("É menor de idade!");
+}
