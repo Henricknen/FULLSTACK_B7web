5cfbf3403d4388d7673179724b4425f663ce8167
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-01 11:55:15 -0300

    switch_e_uma_condicional_que_se_tem'_multiplos'_resultados

---
+++ JAVASCRIPT/javascript_basico/switch/js/script.js
@@ -0,0 +1,18 @@
+let profession = "fullstack";
+
+switch(profession) {
+    case 'front end':
+        console.log('trabalha com html, css, javascript');
+    break;
+    
+    case 'fullstack':
+        console.log('trabalha com html, css, javascript, python, php, sql, laravel, desginer');
+    break;
+
+    case 'back end':
+        console.log('trabalha com php, javascript, python, sql');
+    break;
+
+    default:        // condiçional padrão
+        console.log('eu ultilizo html, css, javascript, python, php, sql, laravel');
+}
