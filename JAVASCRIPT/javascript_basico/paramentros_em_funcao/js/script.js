28adb0bc368a4f837a8eb008d4cbc321b7caf3bf
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-02 13:56:50 -0300

    passando_parametros_em_funcoes

---
+++ JAVASCRIPT/javascript_basico/paramentros_em_funcao/js/script.js
@@ -0,0 +1,13 @@
+function somar(n1, n2) {        // definindo a função com dois valores como parâmetros
+    let resultado = n1 + n2;
+    console.log('Resultado: ' + resultado);
+}
+
+somar(10, 15);      // chamando a função 'somar' passando dois números
+
+function meu_nome(nome, sobrenome) {
+    console.log(`${nome} ${sobrenome}`);
+}
+
+meu_nome("Luis", "Henrique");
+
