c47b907275d4178f8f3b7ef7aa4c6e188f14adc9
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-07 20:35:57 -0300

    criando_loop_for_que_executara_10_vezes_conforme_esta_especificado_em_sua_condicao

---
+++ JAVASCRIPT/javascript_basico/loop_for/js/script.js
@@ -0,0 +1,3 @@
+for(let n = 1; n <= 10; n++) {        // a contagem se iniçia em 1, enquanto n for menor ou igual a 10 o loop será executado, o loop será incrementado
+    console.log('Esta é a '+ n+ '° frase');
+}
