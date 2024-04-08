abcbd3f113d033937b8a8908c9cdd97c4da388cd
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-08 20:31:13 -0300

    criando_estrutura_basica_do_loop_'while'_que_executa_o_codigo_enquanto_a_condicao_for_satisfeita

---
+++ JAVASCRIPT/javascript_basico/loop_while/js/script.js
@@ -0,0 +1,6 @@
+let numero = 0;
+
+while(numero <= 10) {       // 'while' enquanto o número for menor ou igual à 10 será executado o loop
+    console.log(`Esse código está sendo executado ${numero} vezes`);
+    numero++;       // por variável número ser igual á 0, para evitar loop infinito 'numero++' incrementa 1 a cada loop executado
+}
