217b84d2c95c833484bbd8fbaa5bbace84745bb0
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-10 21:05:03 -0300

    ultilizando_'querySelector'_em_arquivo_externo_para_fazer_a_selecao

---
+++ JAVASCRIPT/dom/selecionando_elementos/assets/js/script.js
@@ -0,0 +1,6 @@
+var h1Element = document.querySelector('h1');       // selecionando o elemento <h1>
+
+var divElement = document.querySelector('#teste');      // selecionando a <div> com id 'teste'
+
+console.log(h1Element.textContent);      // imprimindo no 'console'
+console.log(divElement.textContent);
