4d81fa32d823a677a494c499ffcc2a092e8c8af7
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-12 18:49:27 -0300

    alterando_css_via_javascript

---
+++ JAVASCRIPT/dom/manipulando_o_css/assets/js/script.js
@@ -0,0 +1,6 @@
+function clicou() {
+    const li = document.querySelector('li');
+
+    li.style.backgroundColor = '#00F';      // trocando a cor do elemento 'li'
+    li.style.fontSize = '20px';
+}
