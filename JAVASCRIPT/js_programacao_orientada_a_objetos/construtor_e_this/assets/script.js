a9ed1dcd357fca6b1d9f1903c75d167b65eb881f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-14 16:26:53 -0300

    iniciando_codificacao_'poo'_criando_clase_com_constructor_e_this

---
+++ JAVASCRIPT/js_programacao_orientada_a_objetos/construtor_e_this/assets/script.js
@@ -0,0 +1,6 @@
+class Person {      // classe
+    constructor(name, age) {     // construtor reçebendo variáveis 'name' e 'age' como parâmetro
+        this.name = name;       // propriedade 'name' reçebe variável 'name', 'this' se refere ao proprio objeto
+        this.age = age;
+    }
+}
