c70be474e5d100059e80a744400976729a669205
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-20 15:36:02 -0300

    substituindo_o_construtor_por_uma_funcao_especifica_chamada_'graduacao'_no_proprio_objeto

--- JAVASCRIPT/js_programacao_orientada_a_objetos/sistema_luta_programacao_funcional/assets/script.js
+++ JAVASCRIPT/js_programacao_orientada_a_objetos/sistema_luta_programacao_funcional/assets/script.js
@@ -2,9 +2,14 @@ let person = {
     name: 'Luis',
     lastname: 'Henrique',
     age: 33,
-    getFullName() {     // criando função sendo propriedade, ela tem acesso ao objeto 'this'
+    getFullName() {
         return `${this.name} ${this.lastname}`;
+    },
+    graduacao() {
+        console.log('Sou formado em Anánilise e desenvolvemento de sistemas');
     }
 }
+
+person.graduacao();     // antes de executar qualquer coisa tem que chamar a funcao graduação que 'substitui' o construtor
 
 console.log(person.getFullName());
