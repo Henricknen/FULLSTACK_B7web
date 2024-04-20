1f5c6a024b7a4ed1700333b3158df91a97227a17
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-20 16:03:37 -0300

    criando_'heranca'_herdando_as_propriedades_de_outro_objeto

--- JAVASCRIPT/js_programacao_orientada_a_objetos/sistema_luta_programacao_funcional/assets/script.js
+++ JAVASCRIPT/js_programacao_orientada_a_objetos/sistema_luta_programacao_funcional/assets/script.js
@@ -13,3 +13,29 @@ let person = {
 person.graduacao();     // antes de executar qualquer coisa tem que chamar a funcao graduação que 'substitui' o construtor
 
 console.log(person.getFullName());
+
+// ------------------------------ herança ---------------------------------------------------
+
+const defaultUser = {       // modelo 'gerenalista'
+    name: '',
+    email: '',
+    level: 1
+}
+
+let User1 = {       // usuário comum
+    ...defaultUser,     // clonando objeto 'defaultUser'
+    name: 'Luis Henrique',
+    email: 'l.henrick@live.com'
+}
+
+let Adm = {     // usuário 'administrador'
+    ...defaultUser,
+    name: 'Full Stack',
+    email: 'l.henrick@hotmail.com',
+    level: 2        // alterando o 'level'
+}
+
+console.log(Adm);
+
+
+
