209eb97716ab8e712ee1081ac5ff9ca303d942d6
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-21 16:40:07 -0300

    criando_o_cenario_'stage'

--- JAVASCRIPT/js_programacao_orientada_a_objetos/sistema_luta_programacao_funcional/assets/functions.js
+++ JAVASCRIPT/js_programacao_orientada_a_objetos/sistema_luta_programacao_funcional/assets/functions.js
@@ -49,3 +49,34 @@ const createBigMonster = () => {      // personagem 'grande mostro'
         defense: 6
     }    
 }
+
+const stage = {
+    fight1: null,
+    fight2: null,
+    fight1El: null,
+    fight2El: null,
+
+    start(fight1, fight2, fight1El, fight2El) {
+        this.fight1 = fight1;
+        this.fight2 = fight2;
+        this.fight1El = fight1El;
+        this.fight2El = fight2El;
+
+        this.fight1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fight1, this.fight2));
+        this.fight2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fight2, this.fight1));
+
+        this.update();
+    },
+    update() {
+        this.fight1El.querySelector('.name'). innerHTML = `${this.fight1.name} - ${this.fight1.life.toFixed(1)} HP`;
+        let f1Pct = (this.fight1.life / this.fight1.maxLife) * 100;
+        this.fight1El.querySelector('.bar'). style.width = `${f1Pct}%`;
+        
+        this.fight2El.querySelector('.name'). innerHTML = `${this.fight2.name} - ${this.fight2.life.toFixed(1)} HP`;
+        let f2Pct = (this.fight2.life / this.fight2.maxLife) * 100;
+        this.fight2El.querySelector('.bar'). style.width = `${f2Pct}%`;
+    },
+    doAttack(attacking, attacked) {
+
+    }
+}
--- JAVASCRIPT/js_programacao_orientada_a_objetos/sistema_luta_programacao_funcional/assets/script.js
+++ JAVASCRIPT/js_programacao_orientada_a_objetos/sistema_luta_programacao_funcional/assets/script.js
@@ -1,2 +1,9 @@
-const char = createKnight('Luis Henrique');     // criacão do 'cavaleiro'
+const hero = createKnight('Luis Henrique');     // criacão do 'cavaleiro'
 const monster = createLittleMonster();      // criação do 'pequeno mostro'
+
+stage.start(
+    hero,
+    monster,
+    document.querySelector('#hero'),
+    document.querySelector('#monster')
+);
