0b86ae121f4730570c85323a99b36e2b6b5313e2
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-22 11:16:29 -0300

    exemplificando_conceito_de_'sincronismo'_que_executa_uma_linha_por_vez

---
+++ JAVASCRIPT/requisicoes/sincrono_assincrono/assets/script.js
@@ -0,0 +1,5 @@
+        // execução Sicrono
+let nome = "Luis Henrique S F";     // primeiro executa está linha
+let profissional = "Full Stack";        // segundo executa está linha
+
+let perfil = nome + profissional;           // na definição da variável perfil, pelo código ter sido executado de forma sicrona 'nome' e 'profissional' terão valores
