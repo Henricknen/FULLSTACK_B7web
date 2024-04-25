b85b36b1b4818774108988dd57cab6bceaf28db0
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-25 15:03:38 -0300

    ultilizando_metodos_'length'_e_'indexOf'_para_manipular_string

---
+++ JAVASCRIPT/manipulacoes_arrow_ajax/metodo_de_string_length_indexof/assets/script.js
@@ -0,0 +1,7 @@
+let nome = "Luis Henrique S F";
+
+let qtd = nome.length;       // 'length' mostra quantos caracters tem a string nome
+console.log(qtd);
+
+let pos = nome.indexOf('Henrique');     // 'indexOf' mostra a posição que a string iniçia por 'H'
+console.log(pos);
