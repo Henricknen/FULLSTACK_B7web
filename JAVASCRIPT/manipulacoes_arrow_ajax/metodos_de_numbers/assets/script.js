4668e4dc8db1c1ebb7b0eaaf04fa258a94ec3d8f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-04-27 00:14:18 -0300

    ultilizando_as_quatros_principais_funcoes_de_manipulacao_que_'envolve_numeros'

---
+++ JAVASCRIPT/manipulacoes_arrow_ajax/metodos_de_numbers/assets/script.js
@@ -0,0 +1,15 @@
+let n = 10;
+let res = n.toString();     // função toString 'converte' o numero em string
+console.log(res);
+
+let pi = 3.14159265358979323846;
+let ress = pi.toFixed(2);       // 'toFixed' formata o número com a quantidade de casas decimais espeçificadas como parâmetro
+console.log(ress);
+
+let str = '25';
+let resss = parseInt(str) + 5;       // 'parseInt' converte string em número
+console.log(resss);
+
+let float = '99,9';
+let ressss = parseFloat(float) + 1;     // 'parseFloat' preserva os deçimais dos números
+console.log(ressss);
