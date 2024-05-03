3e30c6743c5357e2b33d850d4a0b0757e774e7a6
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-03 09:42:49 -0300

    criando_as_'principais'_funcaoes_matematicas_relacionadas_a_classe_'math'

---
+++ JAVASCRIPT/manipulacoes_arrow_ajax/matematicas/assets/script.js
@@ -0,0 +1,42 @@
+let pi = Math.PI;       // classe 'Math.PI' calcula o número PI
+console.log(pi);
+
+// ------------------------------------------------------------------------------
+
+let num = Math.round(1.56);     // função 'round' arredonda o número
+console.log(num);
+
+// ------------------------------------------------------------------------------
+
+let nume = Math.floor(3.56);     // função 'floor' arredonda o número para baixo
+console.log(nume);
+
+// ------------------------------------------------------------------------------
+
+let numer = Math.ceil(3.56);     // função 'ceil' arredonda o número para cima
+console.log(numer);
+
+// ------------------------------------------------------------------------------
+
+let numero = Math.abs(-5);     // função 'abs' mostra o valor absoluto de -5
+console.log(numero);
+
+// ------------------------------------------------------------------------------
+
+let numeroo = Math.min(5, 50, 20, 70, 100, 3, 52, 9, 500);     // função 'min' mostra o menor valor
+console.log(numeroo);
+
+// ------------------------------------------------------------------------------
+
+let numerooo = Math.max(5, 50, 20, 70, 100, 3, 52, 9, 500);     // função 'max' mostra o maior valor
+console.log(numerooo);
+
+// ------------------------------------------------------------------------------
+
+let numeroooo = Math.random();      // função 'random' retorna um número aleatório entre 0 e 1
+console.log(numeroooo)
+
+// ------------------------------------------------------------------------------
+
+let numerooooo = Math.floor(Math.random() * 100);       // multiplicando o número 'random' aleátorio de 0 a 1 por 100 e arredondado para baixo 'floor' para que o arredondamento não passe de 100
+console.log(numerooooo);
