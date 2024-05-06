f14ceacb756ea3776725b14ece0374940b9a664f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-06 16:14:13 -0300

    funcao_'include'_serve_tanto_para_string_quanto_para_array_serve_para_procurar_determinado_elemento_em_string_ou_array_e_'repeat'_serve_para_fazer_a_repeticao_de_quantas_vezes_precisar

---
+++ JAVASCRIPT/manipulacoes_arrow_ajax/includes_e_repeat/assets/script.js
@@ -0,0 +1,14 @@
+let hardskills = ['javascript', 'php', 'python', 'sql', 'laravel', 'arduino', 'css', 'html'];       // array
+
+console.log(hardskills.includes('javascript'));     // função 'include' retornará 'true' se o elemento que estiver dentra das aspas estiver no array
+
+// ------------------------------------------------------------------------------------------------------
+
+let nome = 'Include';       // string
+
+console.log(nome.includes('c'));        // procurando letra espeçifica 'c' dentra da string
+
+// ------------------------------------------------------------------------------------------------------
+
+let string = 'repetição/';
+console.log( string.repeat(20));     // função 'repeat' fará a string se repeta 20 vezes
