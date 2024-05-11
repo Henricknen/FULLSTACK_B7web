8b11f777d64571861264a6480b3db79eb4195e45
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-11 20:23:34 -0300

    criando_logica_para_pegar_as_teclas_digitadas

--- JAVASCRIPT/bateria/script.js
+++ JAVASCRIPT/bateria/script.js
@@ -2,15 +2,26 @@ document.body.addEventListener('keyup', (event)=> {     // 'document.body' repre
     playSound(event.code.toLowerCase());
 });
 
+document.querySelector('.composer button').addEventListener('click', ()=> {
+    let song = document.querySelector('#input').value;
+
+    console.log("Musica", song);
+});
+
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
     if(audioElement) {
+        audioElement.currentTime = 0;
         audioElement.play();
     }
 
     if(keyElement) {
         keyElement.classList.add('active');
+
+        setTimeout(()=> {
+            keyElement.classList.remove('active');
+        }, 300);
     }
 }
