b1c194db707718446c47762923259dd558600a44
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-11 20:01:03 -0300

    adicionando_efeito_de_cor_nas_teclas_digitas_pelo_usuario

--- JAVASCRIPT/bateria/script.js
+++ JAVASCRIPT/bateria/script.js
@@ -4,8 +4,13 @@ document.body.addEventListener('keyup', (event)=> {     // 'document.body' repre
 
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
     if(audioElement) {
         audioElement.play();
+    }
+
+    if(keyElement) {
+        keyElement.classList.add('active');
     }
 }
