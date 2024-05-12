233ae8549890f2608ad47e7bfaff35a5532f803b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-12 20:42:03 -0300

    concluindo_o_relogio_'digital'_e_inserindo_comentarios_explicativos

---
+++ JAVASCRIPT/relogio_analogico_e_digital/script.js
@@ -0,0 +1,23 @@
+let digitaElement = document.querySelector('.digital');     // seleçionando o elemento do relógio digital
+let sElement = document.querySelector('.p_s');        // selecionando o ponteiro de 'segundo'
+let mElement = document.querySelector('.p_m');        // selecionando o ponteiro de 'minuto'
+let hElement = document.querySelector('.p_h');        // selecionando o ponteiro de 'hora'
+
+function updateClock() {        // função 'updateClock' atualizará o relógio
+    let now = new Date();       // pegando a hora atual
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let secund = now.getSeconds();
+
+    digitaElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(secund)}`;
+}
+
+function fixZero(time) {
+    if(time < 10) {     // enquanto o valor dos segundos, mintutos ou horas forem menor que 0 será acrescentado 0 antes
+        return '0' + time;
+    } else {
+        return time;
+    }
+}
+
+setInterval(updateClock, 1000);      // função 'setInterval' executa a função 'updateClock' a cada segundo '1000'
