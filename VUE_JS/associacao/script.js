29cc7a065d40ad963710364b5059ea11a76a37c4
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-31 20:30:58 -0300

    criando_'associacao'_do_'vue'_do_arquivo_javascript_com_elemento_div_do_html

---
+++ VUE_JS/associacao/script.js
@@ -0,0 +1,7 @@
+let app = new Vue ({        // variável app iniçiando a classe Vue
+    el: '#app',     // objeto json 'el' faz assoçiação com o a div de id 'app' 
+    data: {
+        frase: 'Codificando Vue.js',     // inserção de dados no elmento
+        frase2: 'Framework <strong>JavaScript</strong>'
+    }
+});
