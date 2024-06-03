6e89f39f438d012d524a1f3ba515672c84e297de
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-03 12:30:01 -0300

    'sintaxe_de_template'_executando_diversas_formas_de_'interpolacoes'_que_e_a_insercao_de_variaveis_ou_expressoes__dentro_de_uma_string

---
+++ VUE_JS/interpolacoes/script.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+	el: '#app',
+	data:{
+        titulo: 'Titulo da página',
+        subtitulo: 'Este é o <strong>sub titulo</strong> da página',        // html
+        copyright: 'Todos os direitos reservados',
+
+        profissional : 'Luis Henrique Silva Ferreira',
+
+        carregado: true
+	}
+});
