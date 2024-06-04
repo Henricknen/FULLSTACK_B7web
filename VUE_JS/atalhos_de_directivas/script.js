754744a61c9519a9982130b95ed96c866684bca9
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-04 18:26:35 -0300

    sitaxe_de_template_criando_'atalhos'_de_directivas_v-bind_':'_v-on_'@'

---
+++ VUE_JS/atalhos_de_directivas/script.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+	el: '#app',
+	data: {
+                url: 'https://github.com/Henricknen/FULLSTACK_B7web/tree/main/VUE_JS'
+	},
+        methods: {
+                fazAlgumaCoisa:()=> {
+                        alert("Fez...");
+                }
+        }
+});
