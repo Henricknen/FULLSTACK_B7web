a804ea2a9306d6e51737afafc003b0ed23410c5f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-09 17:38:57 -0300

    'rendenrizando_lista'_pegando_itens_de_objeto

---
+++ VUE_JS/redenrizacao_de_lista/script.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+	el:'#app',
+	data: {
+		objeto: {		// objeto com propriedades
+			nome: 'Luis Henrique S. F.',
+			profissao: 'Programador FullStack',
+			formacao: 'Análise e desenvolvimento de sistemas',
+			cidade: 'Presidente Prudente-SP'
+		}
+	}	
+});
