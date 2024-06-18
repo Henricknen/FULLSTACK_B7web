83066ac815b0a119309b27d80321bf44a99809fe
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-18 19:37:00 -0300

    apresentando_os_itens_da_lista_na_tela

--- VUE_JS/to_do_com_busca/script.js
+++ VUE_JS/to_do_com_busca/script.js
@@ -6,9 +6,26 @@ let busca = {		// variável busca
 	`
 }
 let lista = {
+	props: ['itens'],		// reçebendo as props 'propriedades' itens
 	template: `
 		<div>
-			....
+			<table border = "0" width = "200">
+				<tr>
+					<th>Nome</th>
+					<th>Preço</th>
+				</tr>
+
+				<template v-for = "cat in itens">
+					<tr>
+						<td colspan = "2"><strong>{{ cat.nome }}</strong></td>
+					</tr>
+					<tr v-for = "p in cat.itens">
+						<td>{{ p.nome }}</td>
+						<td>{{ '$' + p.preco }}</td>
+					</tr>
+				</template>
+
+			</table>
 		</div>
 	`
 }
