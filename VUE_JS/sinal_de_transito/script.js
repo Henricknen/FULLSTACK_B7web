87bf25cb770edfdc7e3684e2f4c084656450921c
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-18 18:28:39 -0300

    finalizando_sinal_de_transito_criando_a_funciinalidade_das_luzes

--- VUE_JS/sinal_de_transito/script.js
+++ VUE_JS/sinal_de_transito/script.js
@@ -1,20 +1,34 @@
 let bola = {		// bola componente 'local'
+	props: ['cor'],
 	template: `
-	<div class = "bola">
+	<div class = "bola" v-bind:class = "cor">
 
 	</div>
 	`
 };
 
 Vue.component('sinal', {		// componente 'global' sinal
+	props: ['status'],
 	components: {
 		bola
 	},
 	template: `
 		<div class = "sinal">
-			<bola />
-			<bola />
-			<bola />
+			<template v-if = "status == 'red'">
+				<bola cor = "red"/>
+				<bola />
+				<bola />
+			</template>
+			<template v-else-if = "status == 'yellow'">
+				<bola />
+				<bola cor = "yellow"/>
+				<bola />
+			</template>
+			<template v-else-if = "status == 'green'">
+				<bola />
+				<bola />
+				<bola cor = "green"/>
+			</template>
 		</div>
 	`
 });
@@ -22,14 +36,19 @@ Vue.component('sinal', {		// componente 'global' sinal
 let app = new Vue({
 	el: '#app',
 	data: {
-
+		sinalStatus: 'red'		// 'sinalStatus' indica a cor do sinal
 	},
 	methods: {
 		liberar:function() {
-
+			this.sinalStatus = 'green';
 		},
 		fechar:function() {
-			
+			this.sinalStatus = 'yellow';
+
+			setTimeout(this.fecharSinal, 1000);
+		},
+		fecharSinal:function() {
+			this.sinalStatus = 'red';
 		}
 	}
 }); 
