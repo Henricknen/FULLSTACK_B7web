9cccfbff4e906982ba925e8760dd4b11f38531df
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-06-19 20:32:38 -0300

    finalizando_projeto_implementando_a_'selecao'_dos_elementos_em_estoque_e_fazendo_a_busca_pelo_campo_'input'

--- VUE_JS/to_do_com_busca/script.js
+++ VUE_JS/to_do_com_busca/script.js
@@ -1,17 +1,20 @@
 let busca = {		// variável busca
 	data:function() {
 		return {
-			txt: '',
-			only_stock: false
+			txt: '',		// propriedade do do 'texto digitado'
+			only_stock: false		// propriedade do 'checkbox'
+		}
+	},
+	methods: {
+		buscar:function() {
+			this.$emit('buscar', {txt:this.txt, only_stock:this.only_stock});		// '$emit' envia o evento 'buscar' com os objetos 'txt' e 'only_stok' para o html
 		}
 	},
 	template: `
 		<div>
-			<input type = "text" placeholder = "Busca..." v-model = "txt" /><br/>
-			<input type = "checkbox" id = "only_stock" v-model = "only_stock" />
+			<input type = "text" placeholder = "Busca..." v-model = "txt" v-on:keyup = "buscar" /><br/>
+			<input type = "checkbox" id = "only_stock" v-model = "only_stock" v-on:change = "buscar" />
 			<label for = "only_stock">Mostrar apenas produtos em estoque</label>
-
-			<p>{{ txt }} - {{ only_stock }}</p>
 		</div>
 	`
 }
@@ -60,6 +63,40 @@ let app = new Vue({
 	},
 	created:function() {
 		this.exibidos = this.produtos;
+	},
+	methods: {
+		buscar:function(p) {
+			let novaLista = [];
+
+				for(let i in this.produtos) {		// navegando em produtos para fazer a filtragem
+					let novositens = [];
+
+					for(let w in this.produtos[i].itens) {		// percorrendo os itens da lista para se fazer a filtragem
+							let vai = true;
+
+							if(p.only_stock && this.produtos[i].itens[w].estoque == false) {
+								vai = false;
+							}
+
+							if(p.txt != '' && this.produtos[i].itens[w].nome.toLowerCase(). indexOf(p.txt.toLowerCase()) == -1) {
+								vai = false;
+							}
+
+							if(vai) {
+								novositens.push(this.produtos[i].itens[w]);
+							}
+					}
+
+					if(novositens.length > 0) {
+						novaLista.push({		// adiçionando em 'novalista' a categoria dos itens filtrados
+							nome:this.produtos[i].nome,
+							itens:novositens
+						});
+					}
+				}
+
+				this.exibidos = novaLista;
+		}
 	},
 	components: {		// tranformando variáveis 'busca' e 'lista' em componentes
 		busca,
