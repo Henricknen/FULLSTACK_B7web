31921f7842b66b3cc14c81f24a37c6d5217ec5ee
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-04 23:41:52 -0300

    criando_'types_em_arrays'_utilizando_a_tipagem_seguida_de_[]

---
+++ TYPESCRIPT/types_em_arrays/src/script.ts
@@ -0,0 +1,3 @@
+let nome: string[] = ['Luis', 'Henrique', 'Silva', 'Ferreira'];     // variável com tipagem com [] 'string[]' se refere a um array de strings
+
+let data: number[] = [2024, 6, 4];        // tipagem 'number[]' cria um array de números
