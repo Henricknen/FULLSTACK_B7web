bf33963be05ec783dfa4962e3ec23c9f288a9346
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-11 18:31:34 -0300

    'type_assertions'_e_uma_assistencia_dada_ao_'typescript'_para_ele_entender_especificamente_o_que_e_o_elemento

---
+++ TYPESCRIPT/type_assertions/src/script.ts
@@ -0,0 +1,5 @@
+let idadeField = document.getElementById('idade') as HTMLInputElement;      // type assertions 'as HTMLInputElement' espeçifica o elemento como um 'input'
+
+// ...
+
+console.log( idadeField.value );
