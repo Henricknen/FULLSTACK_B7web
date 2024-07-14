b3ec14034f16cefc05a6f132025030ee8cf8ff3a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-14 14:00:26 -0300

    utilizando_as_chaves_'exclude'_e_'include'_no_tsconfig.json_para_listar_padroes_de_arquivos_e_diretorios_que_serao_excluidos_da_compilacao_e_incluir_arquivos_que_serao_compilados

---
+++ TYPESCRIPT/escolhendo_quais_arquivo_serao_compilados/src/script.ts
@@ -0,0 +1,19 @@
+var profissional = 'Luis Henrique S. F.';
+
+
+function codificando(linguagem_de_programacao: string): string {
+    return 'Estou codificando em: ' + linguagem_de_programacao;
+}
+
+codificando('TypeScript');
+
+
+/*
+
+Para criar um arquivo o arquivo 'tsconfig.json' entre dentro da pasta do arquivo pelo terminal utilize o seguinte comando :
+tsc --init
+
+O comando para verificar o projeto e gerar os arquivos que precisa é:
+tsc
+
+*/
