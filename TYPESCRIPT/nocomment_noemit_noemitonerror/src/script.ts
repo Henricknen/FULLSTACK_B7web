df7aa931e2c63bbb17e2ec12875e4444dcdab8a5
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-15 19:40:17 -0300

    configurando_'removecommets'_como_true_que_irar_remover_os_comentarios_do_arquivo_compilado_js_exemplificadno_'noemit'_e_adicionando_o_'noemmitonerror'_que_nao_compila_nada_se_houver_algum_arquivo_com_erro

---
+++ TYPESCRIPT/nocomment_noemit_noemitonerror/src/script.ts
@@ -0,0 +1,6 @@
+const botao = document.querySelector('button') as HTMLButtonElement;        // botao selecionando o elemento 'button' e 'HTMLButtonElement' espeçifica o item html que virá
+botao.addEventListener('click', ()=> {      // evento de 'click' do botão
+    console.log('clicou!');
+});
+
+// comentarios não apareçerão no arquivo js
---
+++ TYPESCRIPT/nocomment_noemit_noemitonerror/src/script2.ts
@@ -0,0 +1,7 @@
+console.log("Segundo arquivo ts, para teste");
+
+let ano: number = 2024;     // utilizando 'let' para criação de variável, 'const' e 'let' só estão diponiveis apartir do ECMAScript 6
+anoo = "2024";      // criação de variável anoo com erro, sem utilizar 'let' ou 'var'
+
+
+// comentarios não apareçerão no arquivo js
