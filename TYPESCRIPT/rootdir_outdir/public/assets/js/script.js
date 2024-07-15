1e60838c575bb5b6ed5fa792732dc71448c4a25f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-15 18:58:09 -0300

    configurando_o_'rootDir'_e_'outDir'_no_arquivo_tsconfig.json_para_gerenciar_diretorios_de_compilacao_e_saida

---
+++ TYPESCRIPT/rootdir_outdir/public/assets/js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+const botao = document.querySelector('button'); // botao selecionando o elemento 'button' e 'HTMLButtonElement' espeçifica o item html que virá
+botao.addEventListener('click', () => {
+    console.log('clicou!');
+});
---
+++ TYPESCRIPT/rootdir_outdir/src/script2.ts
@@ -0,0 +1,3 @@
+console.log("Segundo arquivo ts, para teste");
+
+let ano: number = 2024;     // utilizando 'let' para criação de variável, 'const' e 'let' só estão diponiveis apartir do ECMAScript 6
