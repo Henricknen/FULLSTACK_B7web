2af97f9c5270587c81bf81928cb7ef9fcd59c945
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-23 21:49:56 -0300

    criando_uma_rota_principal_evitando_criacao_de_rotas_desnecessaria_no_servidor_'server.ts'_e_criando_todas_as_rotas_dentro_de_'index.ts'_router_main

---
+++ NODE_JS/router_main/src/routes/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';          // arquivo de rota prinçipal
+import produtosRouter from './produtos';
+import voosRouter from './voos';
+
+const router = express.Router();
+
+router.use('/produtos', produtosRouter);
+router.use('./voos', voosRouter);
+
+router.get('/ping', (req, res)=> {
+    res.json({ pong: true });
+});
+
+router.get('/', (req, res)=> {
+    let tecnologia: string = "Node.js";
+    let ano: number = 2024;
+
+    res.json({ tecnologia, ano });
+});
+
+
+export default router;
