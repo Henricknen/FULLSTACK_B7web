f98dc3846f0d818cfc9917d69de45d3e56a7a401
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-28 18:51:09 -0300

    criando_'rota'_para_inserir_contatos_fazendo_'verificacoes'_se_nome_existe_ou_e_maior_que_dois

--- NODE_JS/api_listar_contatos/src/routes/index.ts
+++ NODE_JS/api_listar_contatos/src/routes/index.ts
@@ -2,5 +2,14 @@ import express from 'express';
 
 const router = express.Router();
 
+router.post('/contato', (req, res)=> {      // rota 'contato' que irá inserir um novo contato
+    const { name } = req.body;      // pegando o 'name' do body
+
+    if(!name || name.length < 2) {     // se caso 'name' não existir ou for 'menor' que 2 dará erro
+        return res.json({ error: 'Nome precisa ter pelo menos 2 catacters.' });     // 'return' dá a resposta e faz parar a execução
+    }
+
+    res.status(201). json({ contato: name });       // indicando que deu tudo certo
+});
 
 export default router;
