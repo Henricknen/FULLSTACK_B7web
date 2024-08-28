38cd2a668de157835f06d30066442ea284f1b3aa
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-08-28 20:11:53 -0300

    criando_uma_'variavel'_e_explicando_seu_'conceito'_utilizando_comentarios

---
+++ PHP_8.3/introducao/conceito_de_variaveis.php
@@ -0,0 +1,19 @@
+<?php 
+
+$primeiro_nome = 'Luis';        // toda variável é definida iniçiando com o '$'
+
+echo '<h1>Profissional</h1>';
+echo $primeiro_nome;        // fazendo a apresentação na tela do valor armazenado na variável
+
+/*
+
+As variáveis podem conter apenas:
+- Letras;
+- Números;
+- Underline (subrinhado);
+- Variáveis não podem iniçiar com um número;
+- As variáveis diferençia letras maíusculas de letra mínusculas;
+
+*/
+
+?>
