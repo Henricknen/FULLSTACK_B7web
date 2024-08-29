655907ed65141ceb15b7fd2a320635f7712d1894
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-08-29 19:11:05 -0300

    utilizando_o_operador_'.'_para_fazer_concatenacao_de_variavel

---
+++ PHP_8.3/introducao/concatenando_variaveis.php
@@ -0,0 +1,11 @@
+<?php 
+
+$primeiro_nome = 'Luis';
+$segundo_nome = 'Henrique';
+$terceiro_nome = 'Silva';
+$quarto_nome = 'Ferreira';
+
+echo "Meu nome completo é: ";
+echo $primeiro_nome. ' '. $segundo_nome. ' '. $terceiro_nome. ' '. $quarto_nome;        // operador '.' é utilizado para fazer concatenações 
+
+?>
