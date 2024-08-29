3d9a33ce62fe6e217a09788afe9a3aad837284b4
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-08-29 20:44:24 -0300

    'criando'_'imprimindo'_'contando_quantidade_de_caracteres'_de_string

---
+++ PHP_8.3/introducao/trabalhando_com_string.php
@@ -0,0 +1,12 @@
+<?php
+
+$formacao = 'Análise e desenvolvimento de sistemas';        // variável 'formacao' é uma string
+$numeroCaracteres = strlen($formacao);       // função 'strlen' serve para fazer a contagem de caracteres que a variável contém
+
+echo "Sou formado em: ". $formacao;     // 'echo' permite fazer a exibição
+
+echo "<br>";        // '<br>' permite a quebra de linha
+
+echo "A variável Fomação possui, ". $numeroCaracteres. " caracteres";
+
+?>
