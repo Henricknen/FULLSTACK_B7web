5d680d7708463ee38a83f190abc5d2bb64291575
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-08-30 20:16:25 -0300

    realizando_operacoes_matematicas_com_'numero_decimal'

---
+++ PHP_8.3/introducao/numeross.php
@@ -0,0 +1,20 @@
+<?php 
+
+$num1 = 2.00;
+$num2 = 2.80;
+$num3 = 2.94;
+
+echo "A multiplicação de ". $num1. " com ". $num2. " é: ". ($num1 * $num2). "<br>";
+
+echo "A adição de ". $num1. " com ". $num2. " é: ". ($num1 + $num2). "<br>";
+
+echo "A subtração de ". $num1. " com ". $num2. " é: ". ($num1 - $num2). "<br>";
+
+echo "A Divisão de ". $num1. " com ". $num2. " é: ". ($num1 / $num2). "<br>";
+
+echo "Arredondano para cima o número ". $num3. " utilizando função round() e o resultado será: ". round($num3). "<br>";
+
+echo "Arredondano para baixo o número ". $num3. " utilizando função floor() e o resultado será: ". floor($num3). "<br>";
+
+
+?>
