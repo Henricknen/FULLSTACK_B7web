be337c591d234ae24a2734820c9e9f82aebc2d0f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-08-30 17:36:38 -0300

    utilizando_funcao_'strlen'_que_conta_a_quandidade_de_caracteres_de_uma_string_'strpos'_que_busca_a_posicao_de_uma_palavra_em_uma_string_e_'substr'_que_retorna_parte_de_uma_string

--- PHP_8.3/introducao/trabalhando_com_string.php
+++ PHP_8.3/introducao/trabalhando_com_string.php
@@ -1,12 +1,18 @@
 <?php
 
-$formacao = 'Análise e desenvolvimento de sistemas';        // variável 'formacao' é uma string
+$formacao = 'Análise e desenvolvimento de sistemas/ Pós Desenvolvimento Web/ Técnico Informática para internet/ Técnico Eletroeletronica/ Programador Front End';        // variável 'formacao' é uma string
 $numeroCaracteres = strlen($formacao);       // função 'strlen' serve para fazer a contagem de caracteres que a variável contém
+$posicao = strpos($formacao, "Pós Desenvolvimento Web");        // 'strpos' busca a posição da palavra na string
+$parteString = substr($formacao, 0, 38);       // 'substr' retorna a parte de uma string, recebe comoparâmetro e o número de inçio e o número de caracteres que será impresso
 
 echo "Sou formado em: ". $formacao;     // 'echo' permite fazer a exibição
 
 echo "<br>";        // '<br>' permite a quebra de linha
 
-echo "A variável Fomação possui, ". $numeroCaracteres. " caracteres";
+echo "A variável Fomação possui: ". $numeroCaracteres. " caracteres<br>";
+
+echo "Minha útima formação iniçia da posição de número: ". $posicao. "<br>";
+
+echo "A parte da string adiquirida pela função substr é: ". $parteString;
 
 ?>
