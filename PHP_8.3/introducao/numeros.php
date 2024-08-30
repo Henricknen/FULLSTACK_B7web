efe11a594ca231349ea7e81130b42cbfa92690b6
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-08-30 19:51:48 -0300

    utilizando_'numero_inteiro'_para_fazer_operaçoes_matematicas_de_adicao_subtracao_multiplicacao_divisao

---
+++ PHP_8.3/introducao/numeros.php
@@ -0,0 +1,15 @@
+<?php
+
+$numero_int = 2024;       // número não é necessario colocar entre ""
+
+echo "Está codificação foi criada em ". $numero_int. "<br>";
+
+echo "Daqui 6 anos entraremos no ano de ". $numero_int + 6 . "<br>";        // Adição
+
+echo "Há 4 anos atrás estavamos no ano de ". $numero_int - 4 . "<br>";          // Subtração
+
+echo "O dobro desse ano será ". $numero_int * 2 . "<br>";        // Multiplicação
+
+echo "A metade desse ano é ". $numero_int / 2;      // Divisao
+
+?>
