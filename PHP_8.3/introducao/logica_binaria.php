56a4c8d65b5445d586f0a9dc11c06532880a64ce
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-08-31 19:13:14 -0300

    utilizando_tipo_'boolean'_que_representa_verdadeiro_ou_falso

---
+++ PHP_8.3/introducao/logica_binaria.php
@@ -0,0 +1,8 @@
+<?php
+
+$estou_estudando = true;        // variável 'true'
+
+$estou_estudando = !$estou_estudando;        // invertendo o valor da variável
+var_dump($estou_estudando);     // 'var_dump' é uma função de debug, mostrará o tipo e o valor da variável
+
+?>
