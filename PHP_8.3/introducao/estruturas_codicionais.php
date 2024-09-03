621860e65e317b161e3ae4f1065e1884ebf9b133
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-09-03 19:04:05 -0300

    criando_estrutura_codicional_utilizando_'if'_e_operadores_matematicos_'=='_e_'!='

---
+++ PHP_8.3/introducao/estruturas_codicionais.php
@@ -0,0 +1,17 @@
+<?php
+
+$codificando = "PHP";
+
+if($codificando == "PHP") {       // apenas um operador de '=' significa atribuição já dois '==' significa igual
+    echo "Estou codificando PHP";
+
+}
+
+echo "<br>";
+
+$framework = "CODEIGNITER";
+if($framework != "Laravel") {     // '!=' significa diferente
+    echo "Não estou codificando Laravel";
+}
+
+?>
