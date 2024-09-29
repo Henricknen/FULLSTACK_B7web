1a609c86e4c26102bddafc53bfa7f0e666dddadb
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-09-29 19:42:59 -0300

    a_estrutura_de_repeticao_'foreach'_percorre_um_array_e_faz_algo_pra_cada_item_deste_array

---
+++ PHP_8.3/funcoes_e_estruturas_condicionais/estrutura_de_repeticao/foreach.php
@@ -0,0 +1,7 @@
+<?php
+
+$formacao = ['Desenvolvimento Web', 'Análise e desenvolvimento de sistemas', 'Informática para internet', 'Eletroeletrônica', 'Front end'];
+
+foreach($formacao as $minhas_formacoes) {        // 'foreack' percorre o array $'formacao' e atribui um apelido para ele de '$minhas_formacoes'
+    echo $minhas_formacoes. '<br/>';
+}
