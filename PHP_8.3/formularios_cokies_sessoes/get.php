1243a350f6fb7ca95f674f9195c8ee8b9fb7cab0
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-10-03 16:58:35 -0300

    capturando_dados_passados_passados_como_parametro_na_url_com_'GET'

---
+++ PHP_8.3/formularios_cokies_sessoes/get.php
@@ -0,0 +1,5 @@
+<?php
+
+$nome = $_GET['pessoa'];        // 'GET' pega os dados passados na 'query string' da URL e armazena na variável $nome
+
+echo "Olá, $nome";      // comprimentano com 'Olá' o nome recebido por GET
