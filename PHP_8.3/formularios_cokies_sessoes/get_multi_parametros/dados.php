2d309a5c2b831348d9e265a38fab266d5403fa4a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-10-04 18:03:07 -0300

    criando_arquivo_html_que_redireciona_para_multiplos_links_que_contem_valores_diferentes_passados_como_parametros

---
+++ PHP_8.3/formularios_cokies_sessoes/get_multi_parametros/dados.php
@@ -0,0 +1,4 @@
+<?php
+
+$numero_campanha = $_GET['campanha'];       // pegando valor da campanha passado pela 'url'
+echo "Você veio pela campanha ". $numero_campanha;      // imprime menssagem com o numero da capanha recebido pela url
