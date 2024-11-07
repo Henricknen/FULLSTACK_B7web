26f98b735ae5fcec7efd5d64fb3331ce974748f5
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-11-07 18:42:05 -0300

    criando_'primeiro'_componente_que_nada_mais_e_que_uma_parte_reutilizavel_como_uma_funcao

---
+++ REACT_JS/primeiro_componente/src/app/page.tsx
@@ -0,0 +1,11 @@
+function Page() {   // função(componente) retorna um código especifico
+  return (
+    <div>
+      <h1>Criando</h1>
+      <h2 className = "font-bold text-2xl">Componente</h2>    {/* estilizando h2 utilizando 'className' */}
+    </div>
+
+  );
+}
+
+export default Page;    // exportando o função(componente)
