24d5d5fe7827fecf8fb0e219c0877db24a2f61dc
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-11-10 20:21:43 -0300

    organizando_ainda_mais_o_codigo_jsx_criando_funcao_que_mostra_data_'atual'_e_objeto_'data'_que_guarda_todas_informacoes_da_pessoa

--- REACT_JS/jsx/src/components/Person.tsx
+++ REACT_JS/jsx/src/components/Person.tsx
@@ -1,16 +1,24 @@
+const getWeekday = () => {      // função 'getWeekday' retorna o dia da semana
+    return new Intl.DateTimeFormat('pt-BR', { weekday: 'long'}). format(new Date());
+}
+
 export const Person = () => {        // componente 'Person'
-    const name: string = 'Elon Musk';        // variável
-    const avatar: string = 'https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg';
+    const data = {      // objeto 'data' com informações da pessoa
+        name: 'Elon Musk',        // variável 'name'
+        avatar: 'https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg',        // variável 'avatar' que armazena uma imagem
+        roles: ['CEO da Tesla', 'CEO da SpaceX']     // array de 'roles' cargos
+    }
+
     return (
         <>      {/* utilizando 'fragment' é basicamente uma tag sem nome */}
-            <h1>{name}</h1>     {/* exibindo a variável name */}
-            <img src = {avatar} 
-            alt = {name}
+            <h1 style = {{ color: 'red', fontSize: '30px' }}>{data. name} - {getWeekday()}</h1>     {/* utilizando objeto 'data' e sua informação 'name', junto com função 'getWeekday' */}
+            <img src = {data. avatar} 
+            alt = {data. name}
             className = "w-40"
             />
             <ul>
-                <li>CEO da Tesla</li>
-                <li>CEO da SpaceX</li>
+                <li>{data. roles[0]}</li>       {/* pegando o primeiro cargo que se encontra no array 'roles' cargos */}
+                <li>{data. roles[1]}</li>
             </ul>
         </>
     )
