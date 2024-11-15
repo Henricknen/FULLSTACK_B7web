d998abc15e6474ea0e082cb80b3a54e0cbd4f4d4
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-11-14 21:01:09 -0300

    utilizando_o_operador_logico_AND_que_e_representado_por_dois_sinais_'&&'_juntos_e_tem_a_funcao_de_executar_ou_nao_executar_algo_dependendo_de_uma_determinada_condicao

---
+++ REACT_JS/operador_logico_and/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@/components/Card";
+
+const Page = () => {
+  return (
+    <div>
+      <h2 className = "font-bold text-2xl">Codificando</h2>
+      <h1>ReactJs</h1>
+
+      <Card phrase = "Utilizando operador lógico &&" author = "Luis Henrique Silva Ferreira"/>
+    </div>
+  );
+}
+
+export default Page;
