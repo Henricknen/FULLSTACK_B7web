3ff10aa6dbf2ac379663cba824c58b4dfda8ff9b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-11-20 21:58:41 -0300

    'inserindo'_as_informacoes_da_pessoas_na_tabela

--- REACT_JS/tabela/src/components/StudentTable.tsx
+++ REACT_JS/tabela/src/components/StudentTable.tsx
@@ -1,27 +1,40 @@
 import { Student } from "@/types/Student";
 
-
 type Props = {
-    students: Student[];
-}
+  students: Student[];
+};
 
 export const StudentTable = ({ students }: Props) => {
-    return (
-        <div>
-            <table className = "w-full border border-gray-600 rounded-md overflow-hidden">
-                <thead>
-                    <tr className = "text-left border-b border-gray-600 bg-gray-300">
-                        <th className = "p-3">Name</th>
-                        <th className = "p-3">Status</th>
-                        <th className = "p-3">Grade 1</th>
-                        <th className = "p-3">Grade 2</th>
-                        <th className = "p-3">Final Grade</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div>
+      <table className="w-full border border-gray-600 rounded-md overflow-hidden">
+        <thead>
+          <tr className="text-left border-b border-gray-600 bg-gray-300">
+            <th className="p-3">Name</th>
+            <th className="p-3">Status</th>
+            <th className="p-3">Grade 1</th>
+            <th className="p-3">Grade 2</th>
+            <th className="p-3">Final Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((item) => (
+            <tr key={item.id}>
+              <td>
+                <img src={item.avatar} alt={item.name} />
+                <div>
+                  <div>{item.name}</div>
+                  <div>{item.email}</div>
+                </div>
+              </td>
+              <td>{item.active ? "Active" : "Inactive"}</td>
+              <td>{item.grade1}</td>
+              <td>{item.grade2}</td>
+              <td>{(item.grade1 + item.grade2) / 2}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
