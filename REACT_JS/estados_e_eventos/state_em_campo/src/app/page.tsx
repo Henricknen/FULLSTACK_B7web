1d9bc0e3bf51ab9be78c0eabacb4125908b4ac5e
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-11-28 14:10:49 -0300

    finalizando_state_responsavel_por_'manipular'_valor_do_campo_do_input

--- REACT_JS/estados_e_eventos/state_em_campo/src/app/page.tsx
+++ REACT_JS/estados_e_eventos/state_em_campo/src/app/page.tsx
@@ -3,14 +3,23 @@
 import { useState } from 'react';
 
 const Page = () => {  
+  const [nameInput, setNameInput] = useState('');   // state iniciando vazio, 'nameInput' sempre será atualizado com o nome que está no campo
   const  handleBtnClick = () => {
-
+    alert(nameInput);
   }
 
   return (
     <div className = "w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <input type="text" className = "border border-black p-3 text-xl text-black rounded" />
-      <button className = "handleBtnClick">Mostrar valor do campo</button>
+      <input    // campo para digitação do nome
+         type="text"
+         className = "border border-black p-3 text-xl text-black rounded" 
+         placeholder = "Digite seu nome"
+         value = {nameInput}    // associação direta entre o 'value' e o 'state'
+         onChange = {e => setNameInput(e.target.value)}   // utilizando evento 'onChange' para peguar o valor do input
+         />
+
+        <p>Seu nome é: {nameInput}</p>
+        <button onClick={handleBtnClick}>Mostrar valor do campo</button>
     </div>
   );
 
