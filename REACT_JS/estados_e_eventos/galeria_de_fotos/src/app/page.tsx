0875b422be3519f59f922b5696035f8746c87220
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-04 16:46:39 -0300

    finalizando_sistema_'galeria_de_fotos'_que_exibe_as_imagens_em_forma_de_grid_criando_componente_'modal'_e_funcionalidade_de_fechar_'x'_o_modal_aberto

--- REACT_JS/estados_e_eventos/galeria_de_fotos/src/app/page.tsx
+++ REACT_JS/estados_e_eventos/galeria_de_fotos/src/app/page.tsx
@@ -17,9 +17,13 @@ const Page = () => {
     }
   }
 
+  const closeModal = () => {
+    setShowModal(false);  
+  }
+
   return (
     <div className = "mx-2">
-      <h1 className = "text-center text-3xl font-bold my-10">Fotos</h1>
+      <h1 className = "text-center text-3xl font-bold my-10">Galeria de Fotos</h1>
 
       <section className = "container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {photoList.map(item => {
@@ -36,7 +40,7 @@ const Page = () => {
       {showModal &&
         <Modal image = {imageOfModal} closeModal = {closeModal} />
       }
-    </div>
+    </div> 
   );
 }
 
--- REACT_JS/estados_e_eventos/galeria_de_fotos/src/components/Modal.tsx
+++ REACT_JS/estados_e_eventos/galeria_de_fotos/src/components/Modal.tsx
@@ -1,7 +1,17 @@
-export const Modal = () => {
+type Props = {
+    image: string;
+    closeModal: () => void;
+}
+
+export const Modal = ({ image, closeModal }: Props) => {
     return (
-        <div>
-            ....
-        </div>
+        <>      {/* utilizando um 'fragment' para agrupar as duas divs */}
+            <div onClick = {closeModal} className = "fixed left-0 top-0 right-0 bottom-0 flex justify-center items-center bg-black/90">
+                <img src = {`/assets/${image}`} alt = "" className = "max-w-screen max-h-screen" />
+            </div>
+            <div onClick = {closeModal} className = "fixed top-5 right-5 w-10 h-10 cursor-pointer text-white text-5xl">
+                x
+            </div>
+        </>
     );
 }
